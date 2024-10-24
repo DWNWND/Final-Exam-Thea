6bb8dda9e9a6af64ad864ad79aef9ada812f2a5b
Thea <theola51924@stud.noroff.no>
2024-10-24 18:09:12 +0700

    added a color-theme to tailwind and configured it to be visible as vars in css

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,45 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      DEFAULT: "#19161A",
+      black: "#19161A",
+      white: "#ffffff",
+      primary: {
+        green: "#2D521E",
+        blue: "#27346A",
+        light: "#769089",
+        dark: "#696A5C",
+      },
+      comp: {
+        DEFAULT: "#ecebff",
+        purple: "#CCCBDB",
+        gray: "#EFF1EE",
+        green: "#C0D1B9",
+      },
+      success: "#4A9948",
+      warning: "#C78910",
+      danger: "#F44336",
+    },
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    function ({ addBase, theme }) {
+      function extractColorVars(colorObj, colorGroup = "") {
+        return Object.keys(colorObj).reduce((vars, colorKey) => {
+          const value = colorObj[colorKey];
+
+          const newVars = typeof value === "string" ? { [`--color${colorGroup}-${colorKey}`]: value } : extractColorVars(value, `-${colorKey}`);
+
+          return { ...vars, ...newVars };
+        }, {});
+      }
+
+      addBase({
+        ":root": extractColorVars(theme("colors")),
+      });
+    },
+  ],
 };
