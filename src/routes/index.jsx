9e036d63b22ed1e881d3a0ac7a34202c2ccf21ba
Thea <theola51924@stud.noroff.no>
2024-10-24 13:31:52 +0700

    fixed missing routes and exports

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,5 +2,7 @@ export * from "./Checkout";
 export { default as Home } from "./Home";
 export { default as Login } from "./Login";
 export { default as Register } from "./Register";
+export { default as ListingSearch } from "./ListingSearch";
+export { default as ListingSpesific } from "./ListingSpesific";
 export { default as RouteNotFound } from "./RouteNotFound";
 export * from "./User";
