eb52acf7fab5df5fbc33030ea1b5a656214ef74c
Thea <theola51924@stud.noroff.no>
2024-10-26 16:49:37 +0700

    added conditional styling for the backBtn

--- src/components/Layout/Header/index.jsx
+++ src/components/Layout/Header/index.jsx
@@ -3,8 +3,10 @@ import LogoIcon from "../../../assets/logos/holidaize-logo.svg";
 import { useState, useEffect } from "react";
 import HamburgerMenu from "./HamburgerMenu";
 // import styles from "./Header.module.css";
+import useCheckScreenSize from "../../../hooks/useCheckScreenSize";
 
 export default function Header() {
+  const isMobile = useCheckScreenSize();
   const [backPath, setBackPath] = useState("/");
   const location = useLocation();
   const path = location.pathname;
@@ -18,7 +20,7 @@ export default function Header() {
 
   return (
     <header className="flex items-center justify-between p-4 bg-gray-800">
-      <Link to={backPath} className="text-white w-6 h-6"></Link>
+      <Link to={backPath} className={`${!isMobile && "hidden"} ${isMobile && path === "/" ? "hidden" : "text-white w-6 h-6"}`}></Link>
       <Link to="/">
         <img src={LogoIcon} className="h-auto w-auto object-contain" alt="Holidaize logo, click to go to home page" />
       </Link>
