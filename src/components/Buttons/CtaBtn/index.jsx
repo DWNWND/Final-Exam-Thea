7f57185038c13430b85d888a89c27e93756ed2af
Thea <theola51924@stud.noroff.no>
2024-10-26 16:18:51 +0700

    added button components

---
+++ src/components/Buttons/CtaBtn/index.jsx
@@ -0,0 +1,7 @@
+export default function SelectBtn({ innerText, tailw, color, mainCta }) {
+  if (mainCta) {
+    return <button className={`${tailw} w-full p-2 px-20 flex justify-center bg-${color} font-semibold text-xl text-white uppercase hover:shadow-md`}>{innerText}</button>;
+  } else {
+    return <button className={`${tailw} w-full p-2 px-20 flex justify-center border border-solid border-${color} text-${color} uppercase hover:shadow-md`}>{innerText}</button>;
+  }
+}
