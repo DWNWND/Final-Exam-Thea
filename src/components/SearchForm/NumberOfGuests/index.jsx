f53ae9a73fceaaeb955d0eaa5236bb00691c44a5
Thea <theola51924@stud.noroff.no>
2024-10-27 18:56:52 +0700

    fixed up the numberOfGuests with the useForm

--- src/components/SearchForm/NumberOfGuests/index.jsx
+++ src/components/SearchForm/NumberOfGuests/index.jsx
@@ -1,39 +1,38 @@
-import { useReducer } from "react";
+import { useReducer, useEffect } from "react";
 
-// This is our initial state
 const initialState = { guests: 2 };
 
 function reducer(state, action) {
-  // These are actions that can be dispatched
   switch (action.type) {
     case "increment":
-      if (state.guests >= 25) {
-        return { guests: 25 };
-      }
-      return { guests: state.guests + 1 };
+      return { guests: Math.min(state.guests + 1, 25) };
     case "decrement":
-      if (state.guests <= 1) {
-        return { guests: 1 };
-      }
-      return { guests: state.guests - 1 };
-    case "reset":
-      return { guests: 0 };
+      return { guests: Math.max(state.guests - 1, 1) };
+    // case "reset":
+    //   return { guests: 0 };
     default:
       throw new Error();
   }
 }
 
-//remember to have a min and max number of guests
-export default function NumberOfGuests() {
+export default function NumberOfGuests({ register, setValue }) {
   const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    setValue("numberOfGuests", state.guests);
+  }, [state.guests, setValue]);
 
   return (
     <div>
       <label htmlFor="guests">Number of Guests:</label>
       <div>
-        <button onClick={() => dispatch({ type: "decrement" })}>&#45;</button>
-        <input type="number" id="guests" name="guests" min="1" max="25" defaultValue={state.guests} />
-        <button onClick={() => dispatch({ type: "increment" })}>&#43;</button>
+        <button type="button" onClick={() => dispatch({ type: "decrement" })}>
+          &#45;
+        </button>
+        <input {...register("numberOfGuests")} type="number" id="guests" name="guests" min="1" max="25" defaultValue={state.guests} />
+        <button type="button" onClick={() => dispatch({ type: "increment" })}>
+          &#43;
+        </button>
       </div>
     </div>
   );
