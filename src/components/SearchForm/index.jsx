c8bc726331ffbbc69c26f6213eb1d1d5f2447f2b
Thea <theola51924@stud.noroff.no>
2024-10-27 18:56:04 +0700

    added useForm to the searchForm

--- src/components/SearchForm/index.jsx
+++ src/components/SearchForm/index.jsx
@@ -1,14 +1,22 @@
 import LocationLookAhead from "./LocationLookAhead";
 import SelectTravelDates from "./SelectTravelDates";
 import NumberOfGuests from "./NumberOfGuests";
+import { useForm } from "react-hook-form";
 
 export default function SearchForm() {
+  const { register, setValue, handleSubmit } = useForm();
+
+  function onSubmit(data) {
+    console.log(data);
+  }
+
   return (
     <>
       <form onSubmit={handleSubmit(onSubmit)} id="search-travel-form">
-        <LocationLookAhead />
-        <SelectTravelDates />
-        <NumberOfGuests />
+        <LocationLookAhead register={register} setValue={setValue} />
+        <SelectTravelDates register={register} />
+        <NumberOfGuests register={register} setValue={setValue} />
+        <input type="submit" />
       </form>
     </>
   );
