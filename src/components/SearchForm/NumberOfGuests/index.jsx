ab3683b689336642b558d4632ae2fd7f62aba1eb
Thea <theola51924@stud.noroff.no>
2024-10-27 10:40:42 +0700

    added a reduced to set number of guests

--- src/components/SearchForm/NumberOfGuests/index.jsx
+++ src/components/SearchForm/NumberOfGuests/index.jsx
@@ -1,23 +1,39 @@
+import { useReducer } from "react";
 
+// This is our initial state
+const initialState = { guests: 2 };
+
+function reducer(state, action) {
+  // These are actions that can be dispatched
+  switch (action.type) {
+    case "increment":
+      if (state.guests >= 25) {
+        return { guests: 25 };
+      }
+      return { guests: state.guests + 1 };
+    case "decrement":
+      if (state.guests <= 1) {
+        return { guests: 1 };
+      }
+      return { guests: state.guests - 1 };
+    case "reset":
+      return { guests: 0 };
+    default:
+      throw new Error();
+  }
+}
+
+//remember to have a min and max number of guests
 export default function NumberOfGuests() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
     <div>
       <label htmlFor="guests">Number of Guests:</label>
-      <input type="number" id="guests" name="guests" min="1" max="10" />
       <div>
-        <button
-          onClick={() => {
-            dispatch({ type: "decrement" }), decrease(product);
-          }}>
-          &#45;
-        </button>
-        <span>{state.count}</span>
-        <button
-          onClick={() => {
-            dispatch({ type: "increment" }), increase(product);
-          }}>
-          &#43;
-        </button>
+        <button onClick={() => dispatch({ type: "decrement" })}>&#45;</button>
+        <input type="number" id="guests" name="guests" min="1" max="25" defaultValue={state.guests} />
+        <button onClick={() => dispatch({ type: "increment" })}>&#43;</button>
       </div>
     </div>
   );
