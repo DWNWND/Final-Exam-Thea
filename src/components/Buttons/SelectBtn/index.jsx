d42a578aa58a968cab325bb2c059c2eed409aba6
Thea <theola51924@stud.noroff.no>
2024-10-28 18:32:06 +0700

    added the filter/selectBtns to its own components instead of seperately

--- src/components/Buttons/SelectBtn/index.jsx
+++ src/components/Buttons/SelectBtn/index.jsx
@@ -1,7 +1,15 @@
-export default function SelectBtn({ innerText, tailw, color, selected }) {
-  if (selected) {
-    return <button className={`${tailw} w-full p-2 px-20 flex justify-center bg-${color} text-white uppercase hover:shadow-md`}>{innerText}</button>;
-  } else {
-    return <button className={`${tailw} w-full p-2 px-20 flex justify-center border border-solid border-${color} text-${color} uppercase hover:shadow-md`}>{innerText}</button>;
-  }
+export default function SelectBtns({ filters, setFilters }) {
+  return (
+    <div className="flex flex-col gap-2">
+      <button onClick={() => setFilters("unique")} className={`${filters == "unique" ? "bg-primary-blue text-white" : "border border-solid border-color-primary-blue text-primary-blue"} rounded-full w-full p-2 px-20 flex justify-center uppercase hover:shadow-md`}>
+        Unique bookings
+      </button>
+      <button onClick={() => setFilters("luxury")} className={`${filters == "luxury" ? "bg-primary-blue text-white" : "border border-solid border-color-primary-blue text-primary-blue"} rounded-full w-full p-2 px-20 flex justify-center uppercase hover:shadow-md`}>
+        Luxury stays
+      </button>
+      <button onClick={() => setFilters("rating")} className={`${filters == "rating" ? "bg-primary-blue text-white" : "border border-solid border-color-primary-blue text-primary-blue"} rounded-full w-full p-2 px-20 flex justify-center uppercase hover:shadow-md`}>
+        Rating
+      </button>
+    </div>
+  );
 }
