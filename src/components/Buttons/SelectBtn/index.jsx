b160fc23fb136aa4cd7a86b167820441ddbf2caf
Thea <theola51924@stud.noroff.no>
2024-10-28 21:10:10 +0700

    added h2 s on the btns

--- src/components/Buttons/SelectBtn/index.jsx
+++ src/components/Buttons/SelectBtn/index.jsx
@@ -2,13 +2,13 @@ export default function SelectBtns({ filters, setFilters }) {
   return (
     <div className="flex flex-col gap-2">
       <button onClick={() => setFilters("unique")} className={`${filters == "unique" ? "bg-primary-blue text-white" : "border border-solid border-color-primary-blue text-primary-blue"} rounded-full w-full p-2 px-20 flex justify-center uppercase hover:shadow-md`}>
-        Unique bookings
+        <h2>Unique bookings</h2>
       </button>
       <button onClick={() => setFilters("luxury")} className={`${filters == "luxury" ? "bg-primary-blue text-white" : "border border-solid border-color-primary-blue text-primary-blue"} rounded-full w-full p-2 px-20 flex justify-center uppercase hover:shadow-md`}>
-        Luxury stays
+        <h2>Luxury stays</h2>
       </button>
       <button onClick={() => setFilters("rating")} className={`${filters == "rating" ? "bg-primary-blue text-white" : "border border-solid border-color-primary-blue text-primary-blue"} rounded-full w-full p-2 px-20 flex justify-center uppercase hover:shadow-md`}>
-        Rating
+        <h2>Rating</h2>
       </button>
     </div>
   );
