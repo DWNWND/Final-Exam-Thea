2b63eb54c60088956d11d938272951e6ada42a2d
Thea <theola51924@stud.noroff.no>
2024-10-28 21:12:22 +0700

    added a component to render newest listings

---
+++ src/components/Venues/NewestListings/index.jsx
@@ -0,0 +1,38 @@
+import { FaArrowRight } from "react-icons/fa6";
+import { Link } from "react-router-dom";
+
+export default function NewestListings({ venues }) {
+  return (
+    <>
+      <h2 className="font-semibold text-xl text-primary-green uppercase">Our newest Listings</h2>
+      {venues && venues.length >= 2 && (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 ">
+          {venues.slice(0, 10).map((venue) => (
+            <div key={venue.id} className="rounded-lg shadow-md bg-white">
+              <div className="relative">
+                <Link to={"/listing/" + venue.id} className="absolute z-50 flex justify-center items-center rounded-full h-10 w-10 font-bold text-2xl text-white bg-primary-blue top-2 right-2 shadow-md">
+                  <FaArrowRight />
+                </Link>
+                <div className="absolute bg-black bg-opacity-40 w-full h-full rounded-t-lg"></div>
+                <img src={venue.media.length > 0 ? venue.media[0].url : null} alt={venue.name} className="w-full h-48 object-cover rounded-t-lg" />
+                <p className="absolute font-bold text-2xl text-white bottom-2 right-2">kr {venue.price}/night</p>
+              </div>
+              <div className="p-4 flex justify-between">
+                <div>
+                  <h3 className="text-xl font-bold text-black">{venue.name}</h3>
+                  <p className="text-black">
+                    {venue.location.city}, {venue.location.country}
+                  </p>
+                </div>
+                <div>
+                  <p>★ {venue.rating}</p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
+      <Link className="underline text-black">View all listings from this category</Link>
+    </>
+  );
+}
