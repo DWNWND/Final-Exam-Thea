80d3b86900df1fc0cd597a70f6e8e00b089629c2
Thea <theola51924@stud.noroff.no>
2024-10-28 18:32:29 +0700

    started adjusting the api call after fetching all the locations

--- src/components/DataProvider/index.jsx
+++ src/components/DataProvider/index.jsx
@@ -1,10 +1,10 @@
 import useFetch from "../../hooks/useFetch.jsx";
 import { useState, useEffect } from "react";
-import { Outlet } from "react-router-dom";
+// import { Outlet } from "react-router-dom";
 import { createContext } from "react";
 
-// const page = 1;
-// const limit = 10;
+const page = 1;
+const limit = 10;
 // &page=${page}&limit=${limit}
 
 const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
@@ -12,8 +12,8 @@ const sortedByRating = `${apiBaseUrl}?sort=rating&sortOrder=desc`;
 
 export const DataContext = createContext();
 
-export default function DataProvider({ children }) {
-  const { data, isLoading, isError } = useFetch(sortedByRating);
+export function DataProvider({ children }) {
+  const { data, isLoading, isError } = useFetch(`${apiBaseUrl}?page=${page}&limit=${limit}`);
   const [venues, setVenues] = useState([]);
 
   // const location = useLocation();
@@ -22,7 +22,7 @@ export default function DataProvider({ children }) {
   useEffect(() => {
     const venuesArray = data.data;
     setVenues(venuesArray);
-    console.log(venuesArray);
+    // console.log(venuesArray);
   }, [data]);
 
   return <DataContext.Provider value={{ venues, setVenues, isLoading, isError }}>{children}</DataContext.Provider>;
