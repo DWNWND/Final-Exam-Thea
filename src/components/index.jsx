9e1c2740890a0a86d874cda107f2510c3229e300
Thea <theola51924@stud.noroff.no>
2024-10-28 18:31:39 +0700

    fixed the dataprovider export

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,3 +1,3 @@
 export { default as ErrorFallback } from "./ErrorFallback";
-export { default as DataProvider } from "./DataProvider";
+export { DataProvider } from "./DataProvider";
 export { default as Layout } from "./Layout";
