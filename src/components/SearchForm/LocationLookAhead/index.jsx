ad578ecd36c7ee50ff5a4db52f6fc65c028bc9df
Thea <theola51924@stud.noroff.no>
2024-10-28 17:04:52 +0700

    added a promt to choose location from the list when searching for location

--- src/components/SearchForm/LocationLookAhead/index.jsx
+++ src/components/SearchForm/LocationLookAhead/index.jsx
@@ -17,29 +17,39 @@ export default function LocationLookAhead({ register, setValue }) {
   }, [venues]);
 
   const searchResult = searchableLocations.filter((location) => location.toLowerCase().includes(searchQuery.toLowerCase()));
+  const searchQueryAndResultMatches = searchableLocations.some((location) => location.toLowerCase() === searchQuery.toLowerCase());
 
   return (
-    <>
-      <div className="flex justify-between items-center rounded-full border-primary-green border p-2 px-8  bg-white">
-        <input className=" bg-white w-full italic text-primary-green placeholder:text-primary-light" {...register("location")} id="searchInput" type="search" placeholder="Where do you want to go?" aria-label="Search by City" onChange={(e) => setSearchQuery(e.target.value.trim())} />
+    <div className="relative">
+      <div className="flex justify-between items-center rounded-full border-primary-green border px-3  bg-white">
+        <input autoComplete="off" className=" p-2 bg-transparent w-full italic text-primary-green placeholder:text-primary-light" {...register("location")} id="searchInput" type="search" placeholder="Where do you want to go?" aria-label="Search by City" onChange={(e) => setSearchQuery(e.target.value.trim())} />
         <IoIosSearch className="text-2xl" />
       </div>
       {searchableLocations && searchQuery.length > 0 && (
-        <ul>
-          {searchResult
-            .filter((location) => searchQuery.toLowerCase() !== location.toLowerCase())
-            .map((location) => (
-              <li
-                key={location}
-                onClick={() => {
-                  setSearchQuery(location);
-                  setValue("location", location);
-                }}>
-                {location}
-              </li>
-            ))}
-        </ul>
+        <div className={`${!searchQueryAndResultMatches && "bg-white border border-primary-green shadow-md"} rounded-md absolute top-12 w-full p-4`}>
+          {!searchQueryAndResultMatches && (
+            <>
+              <p className="italic text-primary-green mb-2">Please select a location from the list:</p>
+              <div className="line"></div>
+            </>
+          )}
+          <ul className="bg-white">
+            {searchResult
+              .filter((location) => searchQuery.toLowerCase() !== location.toLowerCase())
+              .map((location) => (
+                <li
+                  className="hover:bg-primary-light p-2 cursor-pointer"
+                  key={location}
+                  onClick={() => {
+                    setSearchQuery(location);
+                    setValue("location", location);
+                  }}>
+                  {location}
+                </li>
+              ))}
+          </ul>
+        </div>
       )}
-    </>
+    </div>
   );
 }
