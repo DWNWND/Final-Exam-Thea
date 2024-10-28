7d8704d02aabb510a6f889ec161c855c475b7d85
Thea <theola51924@stud.noroff.no>
2024-10-28 18:34:00 +0700

    started building out the homepage

--- src/routes/Home/index.jsx
+++ src/routes/Home/index.jsx
@@ -1,18 +1,29 @@
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import SearchForm from "../../components/SearchForm";
 import style from "./Home.module.css";
+import SelectBtns from "../../components/Buttons/SelectBtn";
+import { useState } from "react";
+import VenueCards from "../../components/VenueCards";
+import { DataContext } from "../../components/DataProvider";
+import { useContext } from "react";
 
 export default function Home() {
+  const [filters, setFilters] = useState("unique");
+  const { venues } = useContext(DataContext);
+
   return (
     <HelmetProvider>
       <Helmet prioritizeSeoTags>
         <meta name="description" content="" />
         <title>Home | Holidayz</title>
-        {/* <script src="https://kit.fontawesome.com/279950e6bf.js" crossorigin="anonymous"></script> */}
       </Helmet>
       <div>
-        <section className={`${style.searchSection} p-4`}>
+        <section id="searchHeader" className={`${style.searchSection} p-4 h-[80svh] flex justify-center items-center`}>
           <SearchForm />
+        </section>
+        <section id="categorizedListings" className="p-4 flex flex-col gap-4">
+          <SelectBtns filters={filters} setFilters={setFilters} />
+          <VenueCards filters={filters} venues={venues} />
         </section>
       </div>
     </HelmetProvider>
