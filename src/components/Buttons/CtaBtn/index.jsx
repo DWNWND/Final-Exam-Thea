9454a8d66dbb2f5113a65e8330a7e4b556a8ba82
Thea <theola51924@stud.noroff.no>
2024-10-29 11:02:56 +0700

    adjusted the styling of the cta btn

--- src/components/Buttons/CtaBtn/index.jsx
+++ src/components/Buttons/CtaBtn/index.jsx
@@ -1,6 +1,6 @@
 export default function CtaBtn({ innerText, tailw, color, mainCta }) {
   if (mainCta) {
-    return <button className={`${tailw} w-full p-2 px-20 flex justify-center font-semibold text-xl text-white uppercase hover:shadow-md hover:bg-primary-light`}>{innerText}</button>;
+    return <button className={`${tailw} w-full p-2 px-20 flex justify-center font-semibold text-xl text-white uppercase hover:shadow-md hover:bg-primary-light lg:max-w-8`}>{innerText}</button>;
   } else {
     return <button className={`${tailw} w-full p-2 px-20 flex justify-center border border-solid border-${color} text-${color} uppercase hover:shadow-md`}>{innerText}</button>;
   }
