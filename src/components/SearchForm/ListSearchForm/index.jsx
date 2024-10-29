c9da8ce91fe47a698547a578608087109be17b31
Thea <theola51924@stud.noroff.no>
2024-10-29 18:04:18 +0700

    updated the listsearchform for the list page

--- src/components/SearchForm/ListSearchForm/index.jsx
+++ src/components/SearchForm/ListSearchForm/index.jsx
@@ -5,11 +5,13 @@ import { useForm } from "react-hook-form";
 import CtaBtn from "../../Buttons/CtaBtn/index.jsx";
 import MoreFilters from "../MoreFilters/index.jsx";
 import { useSearchStore } from "../../../stores/useSearchStore.js";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import NavBtn from "../../Buttons/NavBtn/index.jsx";
 
 export default function ListSearchForm() {
-  const { register, setValue, getValue, handleSubmit, reset } = useForm();
+  const [openEditSearch, setopenEditSearch] = useState(false);
+  const { register, setValue, getValues, handleSubmit, reset } = useForm();
   const { formData, setFormData } = useSearchStore();
   const navigate = useNavigate();
 
@@ -26,18 +28,30 @@ export default function ListSearchForm() {
     navigate("/search");
   }
 
+  function handleClick() {
+    console.log("clicked");
+    setopenEditSearch(!openEditSearch);
+  }
+
   return (
-    <div className="bg-white p-6 rounded-3xl md:m-10 lg:rounded-3xl lg:pt-20 lg:pl-20 lg:pr-20 md:py-10 shadow-md w-full h-fit">
-      <h1 className="text-center mb-8 text-2xl font-bold uppercase text-primary-green">Book your next trip now</h1>
-      <form className="flex flex-col gap-4 md:gap-8" onSubmit={handleSubmit(onSubmit)} id="search-travel-form">
-        <div className="flex flex-col lg:flex-row gap-4 ">
-          <LocationLookAhead register={register} setValue={setValue} />
-          <SelectTravelDates register={register} setValue={setValue} />
-          <NumberOfGuests register={register} setValue={setValue} />
-          <CtaBtn type="submit" innerText="Search" tailw="mt-4 md:mt-0 rounded-full bg-primary-green" mainCta={true} />
+    <div className="bg-primary-blue p-6 rounded-3xl md:m-10 md:py-10 shadow-md w-full h-fit xl:sticky xl:top-4">
+      <h1 className="text-center text-2xl font-bold text-white">
+        from: {formData.allDatesInRange[0]} <br></br> to: {formData.allDatesInRange.at(-1)}
+      </h1>
+      <p className="text-white text-center my-4">{formData.numberOfGuests} guests</p>
+      <NavBtn clickFunc={handleClick} arrow={true} open={openEditSearch} innerText="Edit search" tailw="rounded my-4" color="white"></NavBtn>
+
+      <form className={`flex flex-col gap-4 md:gap-8 transition-max-height duration-500 ease-in-out overflow-hidden ${openEditSearch ? "max-h-[1000px] opacity-100" : "max-h-0 opacity-0"}`} onSubmit={handleSubmit(onSubmit)} id="search-travel-form">
+        <div className="flex flex-col gap-4 ">
+          <LocationLookAhead register={register} setValue={setValue} color={"primary-blue"} />
+          <SelectTravelDates register={register} setValue={setValue} color={"primary-blue"} />
+          <NumberOfGuests register={register} setValue={setValue} color={"white"} mainSearch={false} />
+          <CtaBtn type="submit" innerText="Update" tailw="mt-4 md:mt-0 rounded-full bg-white" mainCta={false} color={"primary-blue"} />
         </div>
-        <MoreFilters register={register} setValue={setValue} getValue={getValue} />
+        <MoreFilters register={register} setValue={setValue} getValues={getValues} color={"white"} mainSearch={false} />
       </form>
     </div>
   );
 }
+
+// className="flex flex-col gap-4 md:gap-8"
