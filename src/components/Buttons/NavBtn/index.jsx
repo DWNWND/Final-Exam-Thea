6d5d82b8c274d06ee0f7c2927f067177423261e7
Thea <theola51924@stud.noroff.no>
2024-10-29 18:03:40 +0700

    added a second navBtn option

--- src/components/Buttons/NavBtn/index.jsx
+++ src/components/Buttons/NavBtn/index.jsx
@@ -1,3 +1,19 @@
-export default function NavBtn({ innerText, tailw, color }) {
-  return <button className={`${tailw} p-1 px-3 w-full text-sm text-nowrap flex justify-center border border-solid border-${color} text-${color} uppercase hover:shadow-md`}>{innerText}</button>;
+import { IoIosArrowDown } from "react-icons/io";
+import { IoIosArrowUp } from "react-icons/io";
+
+export default function NavBtn({ innerText, tailw, color, clickFunc, arrow, open }) {
+  if (arrow) {
+    return (
+      <div onClick={clickFunc} className={`${tailw} p-1 px-3 w-full text-sm text-nowrap flex items-center justify-center border border-solid border-${color} text-${color} uppercase hover:shadow-md cursor-pointer`}>
+        {innerText}
+        <span className="ml-2">{open ? <IoIosArrowUp /> : <IoIosArrowDown />} </span>
+      </div>
+    );
+  } else {
+    return (
+      <button onClick={clickFunc} className={`${tailw} p-1 px-3 w-full text-sm text-nowrap flex justify-center border border-solid border-${color} text-${color} uppercase hover:shadow-md`}>
+        {innerText}
+      </button>
+    );
+  }
 }
