01b2a3faf051100df7c9ecd872b9873b21989c2a
Thea <theola51924@stud.noroff.no>
2024-10-29 12:17:15 +0700

    adjusted the styling of the list rendering to fit desktop

--- src/components/Venues/ListNewest/index.jsx
+++ src/components/Venues/ListNewest/index.jsx
@@ -6,13 +6,13 @@ export default function ListNewest({ venues }) {
     <>
       <h2 className="font-bold text-2xl text-primary-green uppercase mb-4">Our newest Listings</h2>
       {venues && venues.length >= 2 && (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 ">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 ">
           {venues.slice(0, 10).map((venue) => (
             <VenueCard venue={venue} key={venue.id} />
           ))}
         </div>
       )}
-      <Link className="block mt-4 underline text-black">View all listings from this category</Link>
+      <Link className="md:text-center lg:text-left block mt-4 underline text-black">View all listings from this category</Link>
     </>
   );
 }
