5f1b1c6b4e1ba16895ba9174f13990f97173639b
Thea <theola51924@stud.noroff.no>
2024-10-29 18:03:26 +0700

    adjusted the btn style

--- src/components/Buttons/CtaBtn/index.jsx
+++ src/components/Buttons/CtaBtn/index.jsx
@@ -2,6 +2,6 @@ export default function CtaBtn({ innerText, tailw, color, mainCta }) {
   if (mainCta) {
     return <button className={`${tailw} w-full p-2 px-20 flex justify-center font-semibold text-xl text-white uppercase hover:shadow-md lg:max-w-8`}>{innerText}</button>;
   } else {
-    return <button className={`${tailw} w-full p-2 px-20 flex justify-center border border-solid border-${color} text-${color} uppercase hover:shadow-md`}>{innerText}</button>;
+    return <button className={`${tailw} w-full p-2 px-20 flex justify-center font-semibold border border-solid border-${color} text-${color} uppercase hover:shadow-md`}>{innerText}</button>;
   }
 }
