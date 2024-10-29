2bff854bf3706f3aadbdffcda6bf0f25782bdffa
Thea <theola51924@stud.noroff.no>
2024-10-29 12:15:44 +0700

    renamed the FixedMobileFooter to FixedBtnDisplay

--- src/components/Layout/FixedBtnDisplay/index.jsx
+++ src/components/Layout/FixedBtnDisplay/index.jsx
@@ -2,16 +2,15 @@ import useCheckScreenSize from "../../../hooks/useCheckScreenSize";
 import { Link } from "react-router-dom";
 import { NavBtn } from "../../Buttons";
 
-export default function FixedMobileFooter() {
-  const isMobile = useCheckScreenSize();
-
+export default function FixedBtnDisplay({ isMobile }) {
   // temporarily:
   const isLoggedIn = false;
 
-  if (!isMobile) return null;
+  // if (!isMobile) return null;
+
   return (
-    <div className="fixed bottom-0 w-full bg-white p-4 shadow-2xl z-50">
-      <ul className="flex flex-row gap-2 ">
+    <div className={`${isMobile && "fixed bottom-0 w-full shadow-2xl p-4"} bg-white z-50`}>
+      <ul className="flex flex-row gap-4">
         <li className="w-full">
           <Link to={isLoggedIn ? "/:username/newListing" : "/login"}>
             <NavBtn innerText="List your place" tailw="rounded" color="primary-green" />
@@ -22,7 +21,7 @@ export default function FixedMobileFooter() {
         </li>
         <li className="w-full">
           <Link to="/login">
-            <NavBtn innerText="Login" tailw="rounded-full shadow-md" color="primary-green" />
+            <NavBtn innerText="Login" tailw="rounded-full" color="primary-green" />
           </Link>
         </li>
       </ul>
