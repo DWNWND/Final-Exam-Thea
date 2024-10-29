ab5e8cce7b488801ad7cbe9bc87f852ddefb6e86
Thea <theola51924@stud.noroff.no>
2024-10-29 12:14:34 +0700

    adjusted btn stylign

--- src/components/Buttons/NavBtn/index.jsx
+++ src/components/Buttons/NavBtn/index.jsx
@@ -1,3 +1,3 @@
 export default function NavBtn({ innerText, tailw, color }) {
-  return <button className={`${tailw} p-2 px-4 w-full  text-nowrap flex justify-center border border-solid border-${color} text-${color} uppercase hover:shadow-md`}>{innerText}</button>;
+  return <button className={`${tailw} p-1 px-3 w-full text-sm text-nowrap flex justify-center border border-solid border-${color} text-${color} uppercase hover:shadow-md`}>{innerText}</button>;
 }
