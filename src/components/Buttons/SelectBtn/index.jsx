c4dd038585f897e0554345f5d5b1d322da585a3b
Thea <theola51924@stud.noroff.no>
2024-10-29 12:14:58 +0700

    adjusted styling to fit detsktop

--- src/components/Buttons/SelectBtn/index.jsx
+++ src/components/Buttons/SelectBtn/index.jsx
@@ -1,14 +1,14 @@
 export default function SelectBtns({ filters, setFilters }) {
   return (
-    <div className="flex flex-col gap-2 pt-4 pb-8">
-      <button onClick={() => setFilters("unique")} className={`${filters == "unique" ? "bg-primary-blue text-white" : "border border-solid border-color-primary-blue text-primary-blue"} rounded-full w-full p-2 px-20 flex justify-center uppercase hover:shadow-md`}>
+    <div className="flex flex-col gap-2 pt-4 pb-8 md:flex-row md:justify-center lg:justify-start">
+      <button onClick={() => setFilters("unique")} className={`${filters == "unique" ? "bg-primary-blue text-white" : "border border-solid border-color-primary-blue text-primary-blue"} md:w-auto text-nowrap rounded-full w-full p-2 px-20 flex justify-center uppercase hover:shadow-md`}>
         <h2>Unique bookings</h2>
       </button>
-      <button onClick={() => setFilters("luxury")} className={`${filters == "luxury" ? "bg-primary-blue text-white" : "border border-solid border-color-primary-blue text-primary-blue"} rounded-full w-full p-2 px-20 flex justify-center uppercase hover:shadow-md`}>
+      <button onClick={() => setFilters("luxury")} className={`${filters == "luxury" ? "bg-primary-blue text-white" : "border border-solid border-color-primary-blue text-primary-blue"} md:w-auto text-nowrap rounded-full w-full p-2 px-20 flex justify-center uppercase hover:shadow-md`}>
         <h2>Luxury stays</h2>
       </button>
-      <button onClick={() => setFilters("rating")} className={`${filters == "rating" ? "bg-primary-blue text-white" : "border border-solid border-color-primary-blue text-primary-blue"} rounded-full w-full p-2 px-20 flex justify-center uppercase hover:shadow-md`}>
-        <h2>Rating</h2>
+      <button onClick={() => setFilters("rating")} className={`${filters == "rating" ? "bg-primary-blue text-white" : "border border-solid border-color-primary-blue text-primary-blue"} md:w-auto text-nowrap rounded-full w-full p-2 px-20 flex justify-center uppercase hover:shadow-md`}>
+        <h2>Top rated properties</h2>
       </button>
     </div>
   );
