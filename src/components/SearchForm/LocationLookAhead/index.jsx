8433d1b8768735c7859f241a9f530bc08f62ad4b
Thea <theola51924@stud.noroff.no>
2024-10-29 18:04:40 +0700

    adjusted the locationlookahead to also work on the list search page

--- src/components/SearchForm/LocationLookAhead/index.jsx
+++ src/components/SearchForm/LocationLookAhead/index.jsx
@@ -1,10 +1,10 @@
 import { useState, useContext, useEffect } from "react";
-import { DataContext } from "../../DataProvider";
+// import { DataContext } from "../../DataProvider";
 import { IoIosSearch } from "react-icons/io";
 import { searchableLocations } from "../../../assets/locations/searchableLocations.js";
 
-export default function LocationLookAhead({ register, setValue }) {
-  const { venues } = useContext(DataContext);
+export default function LocationLookAhead({ register, setValue, color }) {
+  // const { venues } = useContext(DataContext);
   const [searchQuery, setSearchQuery] = useState("");
   // const [searchableLocations, setSearchableLocations] = useState([]);
 
@@ -22,15 +22,15 @@ export default function LocationLookAhead({ register, setValue }) {
 
   return (
     <div className="relative w-full flex">
-      <div className="flex justify-between items-center rounded-full border-primary-green border px-3  bg-white w-full">
-        <input autoComplete="off" className=" p-2 bg-transparent w-full placeholder:font-normal placeholder:italic font-semibold text-primary-green placeholder:text-primary-green" {...register("location")} id="searchInput" type="search" placeholder="Where do you want to go?" aria-label="Search by City" onChange={(e) => setSearchQuery(e.target.value.trim())} />
-        <IoIosSearch className="text-2xl text-primary-green" />
+      <div className={`flex justify-between items-center rounded-full border-${color} border px-3  bg-white w-full`}>
+        <input autoComplete="off" className={`p-2 bg-transparent w-full placeholder:font-normal placeholder:italic font-semibold text-${color} placeholder:text-${color}`} {...register("location")} id="searchInput" type="search" placeholder="Where do you want to go?" aria-label="Search by City" onChange={(e) => setSearchQuery(e.target.value.trim())} />
+        <IoIosSearch className={`text-2xl text-${color}`} />
       </div>
       {searchableLocations && searchQuery.length > 0 && (
-        <div className={`${!searchQueryAndResultMatches && "bg-white border border-primary-green shadow-md"} rounded-md absolute top-12 w-full p-4 z-40`}>
+        <div className={`${!searchQueryAndResultMatches && `bg-white border border-${color} shadow-md`} rounded-md absolute top-12 w-full p-4 z-40`}>
           {!searchQueryAndResultMatches && (
             <>
-              <p className="italic text-primary-green mb-2">Please select a location from the list below:</p>
+              <p className={`italic text-${color} mb-2`}>Please select a location from the list below:</p>
               <div className="line"></div>
             </>
           )}
@@ -39,7 +39,7 @@ export default function LocationLookAhead({ register, setValue }) {
               .filter((location) => searchQuery.toLowerCase() !== location.toLowerCase())
               .map((location) => (
                 <li
-                  className="hover:font-semibold p-2 cursor-pointer text-primary-green"
+                  className={`hover:font-semibold p-2 cursor-pointer text-${color}`}
                   key={location}
                   onClick={() => {
                     setSearchQuery(location);
