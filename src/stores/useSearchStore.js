76d2deb6c7c6f7eaafdfda6ce21375c3a85cc078
Thea <theola51924@stud.noroff.no>
2024-10-30 11:36:42 +0700

    updated the searchStore to have preset data

--- src/stores/useSearchStore.js
+++ src/stores/useSearchStore.js
@@ -4,13 +4,28 @@ import { persist, createJSONStorage } from "zustand/middleware";
 export const useSearchStore = create(
   persist(
     (set) => ({
-      formData: {},
+      formData: {
+        allDatesInRange: [],
+        location: "",
+        numberOfGuests: 2,
+        freeWifi: false,
+        petsAllowed: false,
+        freeParking: false,
+        freeBreakfast: false,
+        price100: false,
+        price100to200: false,
+        price200to300: false,
+        price300to400: false,
+        price400to500: false,
+        price500: false,
+      },
       setFormData: (data) => set({ formData: data }),
+      updateFormData: (data) => set((state) => ({ formData: { ...state.formData, ...data } })),
       clearFormData: () => set({ formData: {} }),
     }),
     {
       name: "search-form", // Storage key
-      storage: createJSONStorage(() => localStorage), 
+      storage: createJSONStorage(() => localStorage),
     }
   )
 );
