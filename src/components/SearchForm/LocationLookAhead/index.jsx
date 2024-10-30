46773c3098a28d715e23b940874298875b4f90ef
Thea <theola51924@stud.noroff.no>
2024-10-30 11:38:24 +0700

    added user feedbackand removed old code

--- src/components/SearchForm/LocationLookAhead/index.jsx
+++ src/components/SearchForm/LocationLookAhead/index.jsx
@@ -1,21 +1,9 @@
-import { useState, useContext, useEffect } from "react";
-// import { DataContext } from "../../DataProvider";
+import { useState } from "react";
 import { IoIosSearch } from "react-icons/io";
 import { searchableLocations } from "../../../assets/locations/searchableLocations.js";
 
 export default function LocationLookAhead({ register, setValue, color }) {
-  // const { venues } = useContext(DataContext);
   const [searchQuery, setSearchQuery] = useState("");
-  // const [searchableLocations, setSearchableLocations] = useState([]);
-
-  // useEffect(() => {
-  //   if (venues && venues.length > 0) {
-  //     const locationsSet = new Set(venues.map((venue) => venue.location.city).filter((city) => city));
-  //     const locationsArr = Array.from(locationsSet);
-  //     setSearchableLocations(locationsArr);
-  //   }
-  //   console.log("searchableLocations", searchableLocations);
-  // }, [venues]);
 
   const searchResult = searchableLocations.filter((location) => location.toLowerCase().includes(searchQuery.toLowerCase()));
   const searchQueryAndResultMatches = searchableLocations.some((location) => location.toLowerCase() === searchQuery.toLowerCase());
@@ -23,10 +11,10 @@ export default function LocationLookAhead({ register, setValue, color }) {
   return (
     <div className="relative w-full flex">
       <div className={`flex justify-between items-center rounded-full border-${color} border px-3  bg-white w-full`}>
-        <input autoComplete="off" className={`p-2 bg-transparent w-full placeholder:font-normal placeholder:italic font-semibold text-${color} placeholder:text-${color}`} {...register("location")} id="searchInput" type="search" placeholder="Where do you want to go?" aria-label="Search by City" onChange={(e) => setSearchQuery(e.target.value.trim())} />
+        <input required autoComplete="off" className={`p-2 bg-transparent w-full placeholder:font-normal placeholder:italic font-semibold text-${color} placeholder:text-${color}`} {...register("location")} id="searchInput" type="search" placeholder="Where do you want to go?" aria-label="Search by City" onChange={(e) => setSearchQuery(e.target.value.trim())} />
         <IoIosSearch className={`text-2xl text-${color}`} />
       </div>
-      {searchableLocations && searchQuery.length > 0 && (
+      {searchQuery.length > 0 && (
         <div className={`${!searchQueryAndResultMatches && `bg-white border border-${color} shadow-md`} rounded-md absolute top-12 w-full p-4 z-40`}>
           {!searchQueryAndResultMatches && (
             <>
@@ -35,19 +23,28 @@ export default function LocationLookAhead({ register, setValue, color }) {
             </>
           )}
           <ul className="bg-white">
-            {searchResult
-              .filter((location) => searchQuery.toLowerCase() !== location.toLowerCase())
-              .map((location) => (
-                <li
-                  className={`hover:font-semibold p-2 cursor-pointer text-${color}`}
-                  key={location}
-                  onClick={() => {
-                    setSearchQuery(location);
-                    setValue("location", location);
-                  }}>
-                  {location}
-                </li>
-              ))}
+            {searchResult && searchResult.length > 0 ? (
+              <>
+                {searchResult
+                  .filter((location) => searchQuery.toLowerCase() !== location.toLowerCase())
+                  .map((location) => (
+                    <li
+                      className={`hover:font-semibold p-2 cursor-pointer text-${color}`}
+                      key={location}
+                      onClick={() => {
+                        setSearchQuery(location);
+                        setValue("location", location);
+                        setSearchQuery("");
+                      }}>
+                      {location}
+                    </li>
+                  ))}
+              </>
+            ) : (
+              <li className={`hover:font-semibold p-2 cursor-pointer text-${color}`} onClick={() => setSearchQuery("")}>
+                No location matching "{searchQuery}" found.
+              </li>
+            )}
           </ul>
         </div>
       )}
