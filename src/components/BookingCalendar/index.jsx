b857af015fa95397db4311b4f082a667b8f44155
Thea <theola51924@stud.noroff.no>
2024-11-01 12:25:02 +0700

    added the booking Calender as a separate component

---
+++ src/components/BookingCalendar/index.jsx
@@ -0,0 +1,26 @@
+// https://www.npmjs.com/package/@demark-pro/react-booking-calendar
+import { Calendar } from "@demark-pro/react-booking-calendar";
+import "@demark-pro/react-booking-calendar/dist/react-booking-calendar.css";
+import { useSearchStore } from "../../stores/useSearchStore.js";
+
+export default function BookingCalendar({ reserved }) {
+  const { travelSearchData } = useSearchStore();
+
+  let requestedDates = [travelSearchData.travelDates.startDate, travelSearchData.travelDates.endDate];
+  return (
+    <Calendar
+      classNames={{
+        CalendarContainer: "bg-comp rounded-lg p-4 w-full",
+        DayReservation: "bg-comp-gray",
+        WeekContent: "text-primary-blue",
+        MonthContent: "text-primary-blue",
+        MonthArrowNext: "text-primary-blue",
+        MonthArrowPrev: "text-primary-blue",
+        DayToday: "border-none",
+        DaySelection: "border border-primary-blue bg-transparent",
+      }}
+      selected={requestedDates}
+      reserved={reserved}
+    />
+  );
+}
