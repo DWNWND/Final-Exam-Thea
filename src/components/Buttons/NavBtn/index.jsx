fb9605181e5598a9139c8e818c11844b66cb0d2b
Thea <theola51924@stud.noroff.no>
2024-11-02 12:13:05 +0700

    adjusted btn stylign

--- src/components/Buttons/NavBtn/index.jsx
+++ src/components/Buttons/NavBtn/index.jsx
@@ -4,14 +4,14 @@ import { IoIosArrowUp } from "react-icons/io";
 export default function NavBtn({ innerText, tailw, color, clickFunc, arrow, open }) {
   if (arrow) {
     return (
-      <div onClick={clickFunc} className={`${tailw} p-1 px-3 w-full text-sm text-nowrap flex items-center justify-center border border-solid border-${color} text-${color} uppercase hover:shadow-md cursor-pointer`}>
+      <div onClick={clickFunc} className={`${tailw} p-1 px-3 w-full text-nowrap flex items-center justify-center border border-solid border-${color} text-${color} uppercase hover:shadow-md cursor-pointer`}>
         {innerText}
         <span className="ml-2">{open ? <IoIosArrowUp /> : <IoIosArrowDown />} </span>
       </div>
     );
   } else {
     return (
-      <button onClick={clickFunc} className={`${tailw} p-1 px-3 w-full text-sm text-nowrap flex justify-center border border-solid border-${color} text-${color} uppercase hover:shadow-md cursor-pointer`}>
+      <button onClick={clickFunc} className={`${tailw} p-1 px-3 w-full text-nowrap flex justify-center border border-solid border-${color} text-${color} uppercase hover:shadow-md cursor-pointer`}>
         {innerText}
       </button>
     );
