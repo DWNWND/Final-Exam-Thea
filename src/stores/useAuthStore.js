59064999ac2b6154c14601f756d0a076b7bb8240
Thea <theola51924@stud.noroff.no>
2024-11-02 17:32:26 +0700

    added the user to the authStore and a logOut function

--- src/stores/useAuthStore.js
+++ src/stores/useAuthStore.js
@@ -2,17 +2,21 @@ import { create } from "zustand";
 import { persist } from "zustand/middleware";
 
 // Define the Zustand store with persist middleware
+// Remove the userName(and remember to fix it in the login and register forms)
 const useAuthStore = create(
   persist(
     (set) => ({
       isLoggedIn: false,
       accessToken: null,
       userName: null,
+      user: null,
 
       setIsLoggedIn: () => set({ isLoggedIn: true }),
       setIsLoggedOut: () => set({ isLoggedIn: false }),
       setUserName: (name) => set({ userName: name }),
       setAccessToken: (token) => set({ accessToken: token }),
+      setUser: (user) => set({ user: user }),
+      logOut: () => set({ isLoggedIn: false, accessToken: null, userName: null, user: null }),
     }),
     {
       name: "auth-storage", // name of the storage key in localStorage
