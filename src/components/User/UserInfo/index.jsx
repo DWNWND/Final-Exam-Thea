4a77dc4e000b9322ed426aee1362f5e0b589e066
Thea <theola51924@stud.noroff.no>
2024-11-02 17:32:43 +0700

    started building the profile page

--- src/components/User/UserInfo/index.jsx
+++ src/components/User/UserInfo/index.jsx
@@ -1,7 +1,16 @@
+import useAuthStore from "../../../stores/useAuthStore";
+
 export default function UserInfo() {
+  const { user } = useAuthStore();
+  console.log("user", user);
+
   return (
-    <div>
-      <h1>My Profile</h1>
-    </div>
+    <>
+      <div className="bg-comp-purple rounded-lg p-4">
+        <div className="flex flex-col items-center justify-center rounded bg-white"></div>
+
+        <h1>My Profile</h1>
+      </div>
+    </>
   );
 }
--- src/routes/User/MyProfile/index.jsx
+++ src/routes/User/MyProfile/index.jsx
@@ -1,7 +1,18 @@
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import UserInfo from "../../../components/User/UserInfo";
+import { Link } from "react-router-dom";
+import useAuthStore from "../../../stores/useAuthStore";
+import { useNavigate } from "react-router-dom";
 
 export default function MyProfile() {
+  const { logOut } = useAuthStore();
+  const navigate = useNavigate();
+
+  function handleLogOut() {
+    logOut();
+    navigate("/");
+  }
+
   return (
     <HelmetProvider>
       <Helmet prioritizeSeoTags>
@@ -9,7 +20,23 @@ export default function MyProfile() {
         <title>My Profile | Holidayz</title>
       </Helmet>
       <main className="pt-16">
-        <UserInfo />
+        <section className="flex flex-col p-6 gap-2">
+          <Link to="/" className="text-primary-blue underline text-lg">
+            Book your next stay
+          </Link>
+          <Link to="/user/mySettings" className="text-primary-blue underline text-lg">
+            Register as a venue manager
+          </Link>
+          <Link to="/user/newListing" className="text-primary-blue underline text-lg">
+            Add new listing
+          </Link>
+          <Link className="text-primary-blue underline text-lg" onClick={() => handleLogOut()}>
+            Log out
+          </Link>
+        </section>
+        <section className="flex flex-col p-2 gap-2">
+          <UserInfo />
+        </section>
       </main>
     </HelmetProvider>
   );
