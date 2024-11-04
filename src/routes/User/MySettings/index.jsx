89b924ccf685831e02ab6fe54e252d088769a579
Thea <theola51924@stud.noroff.no>
2024-11-04 16:17:08 +0700

    continued building the edit profile page - successful PUT

--- src/routes/User/MySettings/index.jsx
+++ src/routes/User/MySettings/index.jsx
@@ -6,6 +6,7 @@ import { useState, useEffect } from "react";
 import useFetchUser from "../../../hooks/useFetchUser";
 import useAuthStore from "../../../stores/useAuthStore";
 import * as yup from "yup";
+import Checkbox from "../../../components/Inputs/Checkbox";
 
 export default function MySettings() {
   return (
@@ -14,8 +15,8 @@ export default function MySettings() {
         <meta name="description" content="" />
         <title>My Settings | Holidayz</title>
       </Helmet>
-      <main>
-        <h1>My Settings</h1>
+      <main className="pt-16 p-4">
+        <h1 className="uppercase text-lg text-primary-blue mb-6">Profile settings</h1>
         <SettingsForm />
       </main>
     </HelmetProvider>
@@ -25,17 +26,17 @@ export default function MySettings() {
 // Validation schema for registration
 // remeber to implement validation on email etc.
 const updateSettingsSchema = yup.object().shape({
-  userName: yup.string().min(3, "Username must be at least 3 characters").required("Username is required"),
+  // userName: yup.string().min(3, "Username must be at least 3 characters").required("Username is required"),
   bio: yup.string().min(3, "Bio must be at least 3 characters").required("Bio is required"),
-  avatar: yup.string().url("Please enter a valid url").required("Avatar url is required"),
-  banner: yup.string().url("Please enter a valid url").required("Banner url is required"),
+  avatar: yup.object().shape({
+    url: yup.string().url("Please enter a valid url").required("Avatar url is required"),
+    alt: yup.string().required("Description is required"),
+  }),
+  banner: yup.object().shape({
+    url: yup.string().url("Please enter a valid url").required("Banner url is required"),
+    alt: yup.string().required("Description is required"),
+  }),
   venueManager: yup.boolean(),
-  email: yup.string().email("Please enter a valid email").required("Email is required"),
-  password: yup.string().min(8, "Password must be at least 8 characters").required("Password is required"),
-  confirmPassword: yup
-    .string()
-    .oneOf([yup.ref("password"), null], "Passwords must match")
-    .required("Confirm password is required"),
 });
 
 const url = import.meta.env.VITE_API_BASE_URL;
@@ -67,18 +68,53 @@ function SettingsForm() {
   // const login = useAuthStore((state) => state.login);
 
   const onSubmit = (data) => {
-    console.log("username:", data.userName);
+    console.log("sending form:", data);
+
+    const sendData = async () => {
+      const response = await callApiWith(`${url}/holidaze/profiles/${userName}`, {
+        method: "PUT",
+        body: JSON.stringify(data),
+      });
+      console.log("response:", response);
+      // setUser(response.data);
+    };
+    sendData();
 
     if (!loading && !error) {
     }
   };
 
   //add more levels of userFeedback for the different errorcodes
-  console.log("errors", error);
-
   return (
-    <form>
-      <StringInput type="text" id="userName" label="User name" placeholder={userName} error={errors.userName} register={register}></StringInput>
-    </form>
+    <>
+      {user && (
+        <form className="flex flex-col gap-6">
+          <Checkbox id="venueManager" innerText="Register as a venue manager" checked={user.venueManager} error={errors.venueManager} register={register} color="primary-green"></Checkbox>
+
+          {/* <StringInput type="text" id="userName" label="User name" placeholder="Kari Normann" defaultValue={user.name} error={errors.userName} register={register}></StringInput>
+          <StringInput typ="email" id="email" label="Contact email" placeholder="example@example.no" defaultValue={user.email} error={errors.email} register={register}></StringInput> */}
+          <StringInput type="text" id="bio" label="Bio" placeholder="Something about you" defaultValue={user.bio} error={errors.bo} register={register}></StringInput>
+          <div>
+            <h2 className="mb-2 text-primary-green">Avatar</h2>
+            <div className="flex flex-col gap-4 bg-comp-gray p-4 rounded-xl">
+              <StringInput type="url" id="avatar.url" label="Image url" placeholder="https://example.com/avatar.jpg" defaultValue={user.avatar.url} error={errors.avatar} register={register}></StringInput>
+              <StringInput type="text" id="avatar.alt" label="Image description" placeholder="Something about the avatar image" defaultValue={user.avatar.alt} error={errors.avatar} register={register}></StringInput>
+            </div>
+          </div>
+          <div>
+            <h2 className="mb-2  text-primary-green">Banner</h2>
+            <div className="flex flex-col gap-4 bg-comp-gray p-4 rounded-xl">
+              <StringInput type="url" id="banner.url" label="Image url" placeholder="https://example.com/banner.jpg" defaultValue={user.banner.url} error={errors.banner} register={register}></StringInput>
+              <StringInput type="text" id="banner.alt" label="Image description" placeholder="Something about the banner image" defaultValue={user.banner.alt} error={errors.banner} register={register}></StringInput>
+            </div>
+          </div>
+          <button onClick={handleSubmit(onSubmit)} className="bg-primary-green text-white rounded-full uppercase p-2">
+            Save changes
+          </button>
+          <div className="text-danger">{error}</div>
+          <div className="underline text-center mb-8">Delete user</div>
+        </form>
+      )}
+    </>
   );
 }
