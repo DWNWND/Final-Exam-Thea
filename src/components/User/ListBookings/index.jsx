477fed19647a6ceac43d2129f48004137dbf7ca3
Thea <theola51924@stud.noroff.no>
2024-11-04 13:18:43 +0700

    added a listBookings and listVenues comp for the user

---
+++ src/components/User/ListBookings/index.jsx
@@ -0,0 +1,16 @@
+// import React, { useEffect } from "react";
+// import useFetchUser from "../../../hooks/useFetchUser";
+// import useAuthStore from "../../../stores/useAuthStore";
+
+// const url = import.meta.env.VITE_API_BASE_URL;
+
+export default function ListBookings({ bookings }) {
+
+  console.log("bookings", bookings);
+
+  return (
+    <div>
+      <h1>ListBookings</h1>
+    </div>
+  );
+}
