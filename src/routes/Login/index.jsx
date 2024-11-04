93bb24d141ed9e43292509f870de9f8726b6aa5f
Thea <theola51924@stud.noroff.no>
2024-11-04 22:22:15 +0700

    added checks and renavigation regarding if there is an accesstoken stored or not

--- src/routes/Login/index.jsx
+++ src/routes/Login/index.jsx
@@ -1,7 +1,19 @@
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import LoginForm from "../../components/Forms/Login";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import useAuthStore from "../../stores/useAuthStore.js";
 
 export default function Login() {
+  const { accessToken, userName } = useAuthStore();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (accessToken) {
+      navigate("/user/" + userName);
+    }
+  }, [accessToken]);
+
   return (
     <HelmetProvider>
       <Helmet prioritizeSeoTags>
--- src/routes/User/NewListing/index.jsx
+++ src/routes/User/NewListing/index.jsx
@@ -1,6 +1,18 @@
 import { Helmet, HelmetProvider } from "react-helmet-async";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import useAuthStore from "../../../stores/useAuthStore.js";
 
 export default function NewListing() {
+  const { accessToken } = useAuthStore();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!accessToken) {
+      navigate("/");
+    }
+  }, [accessToken]);
+
   return (
     <HelmetProvider>
       <Helmet prioritizeSeoTags>
