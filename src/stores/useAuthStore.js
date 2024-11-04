caa944180057178bb4720512160fcda7be0453b0
Thea <theola51924@stud.noroff.no>
2024-11-04 13:16:38 +0700

    removed the user from the authStore

--- src/stores/useAuthStore.js
+++ src/stores/useAuthStore.js
@@ -3,19 +3,20 @@ import { persist } from "zustand/middleware";
 
 // Define the Zustand store with persist middleware
 // Remove the userName(and remember to fix it in the login and register forms)
+// you probably dont need to save all the user info in the store, just the token
 const useAuthStore = create(
   persist(
     (set) => ({
       isLoggedIn: false,
       accessToken: null,
       userName: null,
-      user: null,
+      // user: null,
 
       setIsLoggedIn: () => set({ isLoggedIn: true }),
       setIsLoggedOut: () => set({ isLoggedIn: false }),
       setUserName: (name) => set({ userName: name }),
       setAccessToken: (token) => set({ accessToken: token }),
-      setUser: (user) => set({ user: user }),
+      // setUser: (user) => set({ user: user }),
       logOut: () => set({ isLoggedIn: false, accessToken: null, userName: null, user: null }),
     }),
     {
