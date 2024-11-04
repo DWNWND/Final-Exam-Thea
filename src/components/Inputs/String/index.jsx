bc19c207f1b6223a596e1780f1a4fe834f915474
Thea <theola51924@stud.noroff.no>
2024-11-04 13:19:46 +0700

    started working on a separate input comp

---
+++ src/components/Inputs/String/index.jsx
@@ -0,0 +1,10 @@
+export default function StringInput({ type, id, label, placeholder, error, register }) {
+  return (
+    <>
+      <label htmlFor={id} className="block text-primary-green mb-2">
+        {label}
+      </label>
+      <input placeholder={placeholder} type={type} id={id} {...register(id)} className={`placeholder:italic placeholder:font-light text-primary-green border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${error ? "border-danger" : "border-primary-green"}`} />
+    </>
+  );
+}
