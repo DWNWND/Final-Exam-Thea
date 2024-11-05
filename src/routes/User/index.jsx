9e43d4114b9d3927cf7816d21cb31b492345c0ec
Thea <theola51924@stud.noroff.no>
2024-11-05 16:39:18 +0700

    fixed up the pathnames, imports and exports

--- src/routes/User/index.jsx
+++
@@ -1,6 +0,0 @@
-export { default as NewListing } from "./NewListing";
-export { default as MySettings } from "./MySettings";
-export { default as MyProfile } from "./MyProfile";
-export { default as MyListings } from "./MyListings";
-export { default as MyBookings } from "./MyBookings";
-export { default as EditListing } from "./EditListing";
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -5,15 +5,15 @@ export { default as Register } from "./Register";
 export { default as VenueSearch } from "./VenueSearch";
 export { default as VenueSpesific } from "./VenueSpesific";
 export { default as RouteNotFound } from "./RouteNotFound";
-export { default as MyProfile } from "./User/MyProfile";
-export { default as MySettings } from "./User/MySettings";
-export { default as MyBookings } from "./User/MyBookings";
-export { default as NewListing } from "./User/NewListing";
-// export { default as EditVenue } from "./User/EditListing";
-// export { default as BookingSummary } from "./BookingSummary";
-// export { default as BookingDetails } from "./BookingDetails";
-// export { default as BookingPayment } from "./BookingPayment";
-// export { default as BookingSuccess } from "./BookingSuccess";
-// export { default as Contact } from "./Contact";
 
-export * from "./User";
+export { default as NewListing } from "./User/NewListing";
+export { default as MySettings } from "./User/MySettings";
+export { default as MyProfile } from "./User/MyProfile";
+export { default as MyListings } from "./User/MyListings";
+export { default as MyBookings } from "./User/MyBookings";
+export { default as EditListing } from "./User/EditListing";
+
+export { default as BookingCheckout } from "./Booking/BookingCheckout";
+export { default as BookingConfirmation } from "./Booking/BookingConfirmation";
+export { default as BookingDetails } from "./Booking/BookingDetails";
+export { default as BookingSummary } from "./Booking/BookingSummary";
