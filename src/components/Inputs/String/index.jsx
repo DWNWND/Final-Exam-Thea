b63311a64bbe3df95b6515b85ca3f307a002747d
Thea <theola51924@stud.noroff.no>
2024-11-05 10:21:19 +0700

    changes the name of the searchform folder

--- src/components/Inputs/String/index.jsx
+++ src/components/Inputs/String/index.jsx
@@ -1,10 +1,24 @@
-export default function StringInput({ type, id, label, placeholder, error, register, defaultValue }) {
+import { useState } from "react";
+import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
+
+export default function StringInput({ type, id, label, placeholder, error, register, defaultValue, errorMessage }) {
+  const [showPassword, setShowPassword] = useState(false);
+
+  // Toggle password visibility
+  const togglePasswordVisibility = () => setShowPassword((prev) => !prev);
+
   return (
-    <div className="">
+    <div className="relative">
       <label htmlFor={id} className="block text-primary-green mb-2">
         {label}
       </label>
-      <input placeholder={placeholder} type={type} id={id} defaultValue={defaultValue} {...register(id)} className={`${defaultValue && "focus:border-primary-green active:border-primary-green "}placeholder:italic placeholder:font-light font-light text-comp-green border active:text-primary-green focus:text-primary-green active:font-normal focus:font-normal border-comp-gray rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${error && "border-danger"}`} />
+      <input placeholder={placeholder} type={showPassword && type === "password" ? type : "text"} id={id} defaultValue={defaultValue} {...register(id)} className={`${defaultValue && "focus:border-primary-green active:border-primary-green"} focus:border-primary-green active:border-primary-green placeholder:italic placeholder:font-light font-light text-primary-green border active:text-primary-green focus:text-primary-green active:font-normal focus:font-normal border-comp-greenDark rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${error && "border-danger"}`} />
+      {type === "password" && (
+        <div onClick={togglePasswordVisibility} className="absolute right-0 top-8 cursor-pointer text-primary-green h-9 w-9 flex items-center justify-center">
+          {showPassword ? <AiOutlineEyeInvisible size={20} /> : <AiOutlineEye size={20} />}
+        </div>
+      )}
+      {error && <p className="text-danger text-xs mt-1">{errorMessage}</p>}
     </div>
   );
 }
--- src/routes/VenueSearch/index.jsx
+++ src/routes/VenueSearch/index.jsx
@@ -1,5 +1,5 @@
 import { Helmet, HelmetProvider } from "react-helmet-async";
-import ListSearchForm from "../../components/Forms/SearchForm/ListSearchForm";
+import ListSearchForm from "../../components/Forms/SearchTravel/ListSearchForm/index.jsx";
 import { DataContext } from "../../components/DataProvider";
 import { useContext } from "react";
 import ListSearch from "../../components/Venues/ListSearch";
@@ -16,16 +16,16 @@ export default function VenueSearch() {
         <title>Search| Holidayz</title>
         {/* add search details */}
       </Helmet>
-      <main className="flex flex-col xl:flex-row p-4 xl:gap-8">
-      <DataProvider>
-        <section className="max-w-md flex justify-center">
-          <ListSearchForm />
-        </section>
-        <section className="py-12 w-full">
-          <h1 className="font-bold text-xl text-black">Results for {travelSearchData.location}</h1>
-          <ListSearch/>
-        </section>
-      </DataProvider>
+      <main className="flex flex-col lg:flex-row p-4 xl:gap-8 pt-20">
+        <DataProvider>
+          <section className="lg:max-w-md flex justify-center">
+            <ListSearchForm />
+          </section>
+          <section className="py-12 w-full">
+            <h1 className="font-bold text-xl text-black">Results for {travelSearchData.location}</h1>
+            <ListSearch />
+          </section>
+        </DataProvider>
       </main>
     </HelmetProvider>
   );
