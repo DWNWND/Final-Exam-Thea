83dde2107884438fd100cef951cac3306dfc438f
Thea <theola51924@stud.noroff.no>
2024-11-05 11:17:27 +0700

    fixed the view passrowd

--- src/components/Inputs/String/index.jsx
+++ src/components/Inputs/String/index.jsx
@@ -12,7 +12,7 @@ export default function StringInput({ type, id, label, placeholder, error, regis
       <label htmlFor={id} className="block text-primary-green mb-2">
         {label}
       </label>
-      <input placeholder={placeholder} type={showPassword && type === "password" ? type : "text"} id={id} defaultValue={defaultValue} {...register(id)} className={`${defaultValue && "focus:border-primary-green active:border-primary-green"} focus:border-primary-green active:border-primary-green placeholder:italic placeholder:font-light font-light text-primary-green border active:text-primary-green focus:text-primary-green active:font-normal focus:font-normal border-comp-greenDark rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${error && "border-danger"}`} />
+      <input placeholder={placeholder} type={showPassword && type === "password" ? "text" : type} id={id} defaultValue={defaultValue} {...register(id)} className={`${defaultValue && "focus:border-primary-green active:border-primary-green"} focus:border-primary-green active:border-primary-green placeholder:italic placeholder:font-light font-light text-primary-green border active:text-primary-green focus:text-primary-green active:font-normal focus:font-normal border-comp-greenDark rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${error && "border-danger"}`} />
       {type === "password" && (
         <div onClick={togglePasswordVisibility} className="absolute right-0 top-8 cursor-pointer text-primary-green h-9 w-9 flex items-center justify-center">
           {showPassword ? <AiOutlineEyeInvisible size={20} /> : <AiOutlineEye size={20} />}
