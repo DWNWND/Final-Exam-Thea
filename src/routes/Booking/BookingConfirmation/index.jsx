b08cb8522b240c50daf36ed8645836a724ce71d3
Thea <theola51924@stud.noroff.no>
2024-11-06 18:19:33 +0700

    added a sceleton to the booking confirmation route

--- src/routes/Booking/BookingConfirmation/index.jsx
+++ src/routes/Booking/BookingConfirmation/index.jsx
@@ -1,7 +1,18 @@
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import { useNavigate } from "react-router-dom";
+import useAuthStore from "../../../stores/useAuthStore.js";
+
 export default function BookingConfirmation() {
+  const { accessToken, userName } = useAuthStore();
+  const navigate = useNavigate();
+
   return (
-    <div>
-      <h1>BookingConfirmation</h1>
-    </div>
+    <HelmetProvider>
+      <Helmet prioritizeSeoTags>
+        <meta name="description" content="" />
+        <title>Booking details | Holidayz</title>
+      </Helmet>
+      <main className="pt-20 p-4"></main>
+    </HelmetProvider>
   );
 }
