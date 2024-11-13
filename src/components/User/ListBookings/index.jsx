c864dcc5b159099a752554f36592b83b98505ce4
Thea <theola51924@stud.noroff.no>
2024-11-13 20:37:36 +0700

    adjusted styling

--- src/components/User/ListBookings/index.jsx
+++ src/components/User/ListBookings/index.jsx
@@ -4,10 +4,10 @@ import { Link } from "react-router-dom";
 export default function ListBookings({ bookings, maxVenuesShown }) {
   return (
     <div className="flex flex-col gap-2">
-      <h2 className="font-bold text-2xl text-primary-blue uppercase">My bookings</h2>
+      <h2 className="font-bold text-2xl md:text-3xl text-primary-blue uppercase ">My bookings</h2>
       <p className="text-black">{`Showing ${bookings.length} of ${bookings.length} bookings`}</p>
       {bookings && bookings.length >= 1 && (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-4 ">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 2xl:grid-cols-3 gap-4 ">
           {bookings.slice(0, maxVenuesShown).map((booking) => (
             <VenueCard venue={booking.venue} key={booking.id}  myBookings={true} />
           ))}
--- src/components/User/ListVenues/index.jsx
+++ src/components/User/ListVenues/index.jsx
@@ -2,15 +2,14 @@ import VenueCard from "../../Venues/VenueCard";
 import { Link } from "react-router-dom";
 
 export default function ListVenues({ venues, maxVenuesShown }) {
-
   return (
-    <div className="flex flex-col gap-2">
-      <h2 className="font-bold text-2xl text-primary-green uppercase">My active listings</h2>
+    <div className="flex flex-col gap-2 bg-comp-green shadow-md p-8 rounded-lg">
+      <h2 className="font-bold text-2xl md:text-3xl text-primary-green uppercase">My active listings</h2>
       <p className="text-black">{`Showing ${venues.length} of ${venues.length} venues`}</p>
       {venues && venues.length >= 2 && (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-4 ">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 2xl:grid-cols-3 gap-4 ">
           {venues.slice(0, maxVenuesShown).map((venue) => (
-            <VenueCard venue={venue} key={venue.id} myVenues={true}/>
+            <VenueCard venue={venue} key={venue.id} myVenues={true} />
           ))}
         </div>
       )}
