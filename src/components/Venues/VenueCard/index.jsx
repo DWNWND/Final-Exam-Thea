0bb35bb58200c40362b9a36f65e9964530ad068e
Thea <theola51924@stud.noroff.no>
2024-11-13 20:36:10 +0700

    adjusted the stylin g and hoverstates of the venuecard

--- src/components/Venues/VenueCard/index.jsx
+++ src/components/Venues/VenueCard/index.jsx
@@ -2,45 +2,48 @@ import ArrowRightBtn from "../../Buttons/ArrowRightBtn";
 import SquareBtn from "../../Buttons/SquareBtn";
 import { Link } from "react-router-dom";
 
-//the Link cannot be a descendant of another Link (ex. the ArrowRightBtn)
 export default function VenueCard({ venue, myVenues = false, myBookings = false }) {
   return (
-    <Link to={"/venue/" + venue.id} className="rounded-lg shadow-sm bg-white hover:shadow-lg transition duration-300 ease-in-out">
+    <div className="rounded-lg shadow-sm bg-white hover:shadow-lg transition duration-300 ease-in-out relative flex flex-col">
+      <Link to={"/venue/" + venue.id} className="h-full w-full absolute opacity-20 hover:opacity-0  transition duration-300 ease-in-out z-30 rounded-lg">
+        <div className={`bg-black w-full rounded-t-lg hover:bg-opacity-0 h-48`}></div>
+      </Link>
       <div className="relative">
-        <ArrowRightBtn href={"/venue/" + venue.id} myVenues={myVenues} myBookings={myBookings} />
-        <div className={`absolute bg-black bg-opacity-20 w-full h-full rounded-t-lg hover:bg-opacity-0 transition duration-300 ease-in-out `}></div>
+        <ArrowRightBtn href={"/venue/" + venue.id} myVenues={myVenues} myBookings={myBookings} tailw="z-40" />
         <img src={venue.media.length > 0 ? venue.media[0].url : null} alt={venue.media.length > 0 ? venue.media[0].alt : null} className={`w-full h-48 object-cover rounded-t-lg`} />
-        <p className="absolute font-bold text-2xl text-white bottom-2 right-2">kr {venue.price}/night</p>
+        <p className="absolute font-bold text-2xl text-white bottom-2 right-2 z-40">kr {venue.price}/night</p>
       </div>
-      <div className="p-4 flex justify-between">
-        <div>
-          <h3 className="text-xl font-bold text-black hover:underline">{venue.name}</h3>
-          <p className="text-black">
-            {venue.location.city}, {venue.location.country}
-          </p>
+      <div className="p-4 flex flex-col gap-4">
+        <div className="flex justify-between">
+          <div>
+            <h3 className="text-xl font-bold text-black hover:underline">{venue.name}</h3>
+            <p className="text-black">
+              {venue.location.city}, {venue.location.country}
+            </p>
+          </div>
+          <div>
+            <p>★ {venue.rating}</p>
+          </div>
         </div>
-        <div>
-          <p>★ {venue.rating}</p>
+        <div className="flex flex-col gap-2 ">
+          {/* fix these links */}
+          {myVenues && !myBookings && (
+            <>
+              <Link to={`/user/edit/listing`} className="z-40">
+                <SquareBtn innerText="Edit listing" tailw="hover:bg-comp-gray bg-opacity-50 lowercase" bgColor="white" textColor="primary-green" />
+              </Link>
+              <Link to={`/user/:listingId/occupancy`} className="z-40">
+                <SquareBtn innerText="Check occupancy" tailw="hover:bg-comp-gray bg-opacity-50 lowercase" bgColor="white" textColor="primary-green" />
+              </Link>
+            </>
+          )}
+          {!myVenues && myBookings && (
+            <Link to={`/user/:bookingId/cancel`} className="z-40">
+              <SquareBtn innerText="Cancel booking" tailw="hover:bg-comp bg-opacity-50 lowercase" bgColor="white" textColor="primary-blue" borderColor="primary-blue" />
+            </Link>
+          )}
         </div>
       </div>
-      <div className="p-2 flex flex-col gap-2 ">
-        {/* fix these links */}
-        {myVenues && !myBookings && (
-          <>
-            <Link to={`/user/edit/listing`}>
-              <SquareBtn innerText="Edit listing" tailw="hover:bg-comp-gray bg-opacity-50 lowercase" bgColor="white" textColor="primary-green" />
-            </Link>
-            <Link to={`/user/:listingId/occupancy`}>
-              <SquareBtn innerText="Check occupancy" tailw="hover:bg-comp-gray bg-opacity-50 lowercase" bgColor="white" textColor="primary-green" />
-            </Link>
-          </>
-        )}
-        {!myVenues && myBookings && (
-          <Link to={`/user/:bookingId/cancel`}>
-            <SquareBtn innerText="Cancel booking" tailw="hover:bg-comp bg-opacity-50 lowercase" bgColor="white" textColor="primary-blue" borderColor="primary-blue" />
-          </Link>
-        )}
-      </div>
-    </Link>
+    </div>
   );
 }
