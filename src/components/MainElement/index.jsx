775c803cc36afaa06f0502f2e9d52c5da8ba895a
Thea <theola51924@stud.noroff.no>
2024-11-14 12:29:13 +0700

    adjusted the styling of the backdrop

--- src/components/MainElement/index.jsx
+++ src/components/MainElement/index.jsx
@@ -3,26 +3,18 @@ import { useContext } from "react";
 
 export default function MainElement({ children, tailw, homePage = false }) {
   const { isMenuOpen, setIsMenuOpen } = useContext(OpenMenuContext);
+  
 
   return (
-    <div className={`relative`}>
+    <div className="relative">
       <div
-        className={`fixed inset-0 bg-black bg-opacity-50 z-40 transition-opacity duration-500 ease-in-out ${isMenuOpen ? "opacity-100 translate-y-0" : "opacity-0 translate-y-full"}`}
+        className={`inset-0 bg-black bg-opacity-10 transition-opacity duration-500 ease-in-out ${isMenuOpen ? "fixed opacity-100 z-40 pointer-events-auto" : "opacity-0 pointer-events-none"}`}
         onClick={() => setIsMenuOpen(false)} // Optional: Close the menu when clicking the backdrop
       ></div>
 
-      <main className={`${!homePage && "pt-24 p-4 pb-12 md:px-8"} min-h-screen bg-desatBlue ${tailw}`}>{children}</main>
+      <main className={`${!homePage && "pt-24 p-4 pb-12 md:px-8"} transition duration-1000 ease-in-out  min-h-screen bg-desatBlue ${tailw} ${isMenuOpen ? "blur-sm" : "blur-none"}`}>{children}</main>
     </div>
   );
 }
-
-// import { OpenMenuContext } from "../../contexts";
-// import { useContext } from "react";
-
-// export default function MainElement({ children, tailw, homePage = false }) {
-//   const { isMenuOpen, setIsMenuOpen } = useContext(OpenMenuContext);
-
-//   return <main className={`${isMenuOpen && "opacity-30 blur-sm"} ${!homePage && "pt-24 p-4 pb-12 md:px-8"} min-h-svh bg-desatBlue ${tailw}`}>{children}</main>;
-// }
 
 //i need pt-24 for profile page
