26c0bd4d56d13d71610b71ad28a3d0785d5086d1
Thea <theola51924@stud.noroff.no>
2024-11-16 14:20:27 +0700

    added a timer to the skeleton loader to make it appear in dev

--- src/components/Venues/VenueCard/index.jsx
+++ src/components/Venues/VenueCard/index.jsx
@@ -2,19 +2,28 @@ import ArrowRightBtn from "../../Buttons/ArrowRightBtn";
 import SquareBtn from "../../Buttons/SquareBtn";
 import { Link } from "react-router-dom";
 import useAuthStore from "../../../stores/useAuthStore";
+import { useEffect, useState } from "react";
 
-export default function VenueCard({ venue, loading = true, myVenues = false, myBookings = false }) {
+export default function VenueCard({ venue, isLoading, setIsLoading, myVenues = false, myBookings = false }) {
   const { userName, venueManager } = useAuthStore();
 
-  if (loading) {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false); // Simulate data being loaded after 2 seconds
+    }, 2000);
+
+    return () => clearTimeout(timer); // Cleanup timer on unmount
+  }, []);
+
+  if (isLoading) {
     // Render skeleton loader
     return (
       <div className="rounded-lg shadow-sm bg-white relative flex flex-col">
         <div className="animate-pulse">
-          <div className="h-48 bg-comp rounded-t-lg"></div>
+          <div className="h-48 bg-comp-gray rounded-t-lg"></div>
           <div className="p-4 flex flex-col gap-4 ">
-            <div className="h-6 bg-comp rounded w-3/4"></div>
-            <div className="h-4 bg-comp rounded w-1/2"></div>
+            <div className="h-6 bg-comp-gray rounded w-3/4"></div>
+            <div className="h-4 bg-comp-gray rounded w-1/2"></div>
             {/* <div className="h-6 bg-comp rounded w-full"></div>
             <div className="h-6 bg-comp rounded w-full"></div> */}
           </div>
