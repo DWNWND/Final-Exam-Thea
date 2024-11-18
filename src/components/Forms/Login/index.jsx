632a91f8a3afc942860c14653a07649192fbe004
Thea <theola51924@stud.noroff.no>
2024-11-18 12:54:53 +0700

    added a setErrorMessage with custom messages instead of setErrorCode and conditional rendering in the component

--- src/components/Forms/Login/index.jsx
+++ src/components/Forms/Login/index.jsx
@@ -12,8 +12,6 @@ import SmallLoader from "../../SmallLoader";
 import { useSearchStore } from "../../../stores/useSearchStore.js";
 import { useNavigationStore } from "../../../stores/useNavigationStore.js";
 
-// Validation schema
-// remeber to implement validation on email etc.
 const loginSchema = yup.object().shape({
   email: yup.string().required("Email is required"),
   password: yup.string().required("Password is required"),
@@ -23,10 +21,10 @@ export default function LoginForm() {
   const { login, loading, error } = useAuth();
   const { userName } = useAuthStore();
   const navigate = useNavigate();
-  const [errorCode, setErrorCode] = useState(null);
   const { selectedVenue } = useSearchStore();
   const getLastPreviousRoute = useNavigationStore((state) => state.getLastPreviousRoute);
   const lastPreviousRoute = getLastPreviousRoute();
+  const [errorMessage, setErrorMessage] = useState("");
 
   const {
     register,
@@ -37,21 +35,30 @@ export default function LoginForm() {
   } = useForm({ mode: "onChange", resolver: yupResolver(loginSchema) });
 
   const onSubmit = async (data) => {
+    setErrorMessage("");
     const result = await login(data.email, data.password);
 
-    // Check if there was an error
     if (!loading && !error && result.success) {
-      // Assume login returns an object with a success property
       if (selectedVenue && lastPreviousRoute === `/venue/${selectedVenue.id}`) {
         navigate("/booking/details");
       } else {
         navigate("/user/" + userName);
       }
     } else if (!result.success) {
-      setErrorCode(result.error.statusCode);
+      if (result.error.statusCode === 401 || result.error.statusCode === 400) {
+        setErrorMessage("Wrong email or password");
+      }
+      if (result.error.statusCode === 404) {
+        setErrorMessage("User not found");
+      }
+      if (result.error.statusCode === 500) {
+        setErrorMessage("Server error, please try again later");
+      }
+      if (result.error.statusCode !== 400 && result.error.statusCode !== 401 && result.error.statusCode !== 404 && result.error.statusCode !== 500) {
+        setErrorMessage("Unknown error, please try again later");
+      }
 
-      // Handle error feedback if necessary (optional)
-      console.log("Login failed:", result.error || error); // Log error for debugging
+      console.log("Login failed:", result.error || error);
       console.log("error code", result.error.statusCode);
     }
   };
@@ -60,13 +67,13 @@ export default function LoginForm() {
     <div className="mx-auto w-full flex items-center flex-col max-w-[50rem] m-4 p-8 bg-white rounded-lg shadow-sm">
       <h1 className="text-2xl mb-6 uppercase text-primary-green w-full">Login</h1>
       <form className="w-full flex flex-col gap-6" onSubmit={handleSubmit(onSubmit)}>
-        <StringInput type="email" id="email" label="Email address" placeholder="example@example.com" register={register} setErrorCode={setErrorCode} errorMessage={errors.email && errors.email.message} trigger={trigger} watch={watch} />
-        <StringInput type="password" id="password" label="Password" placeholder="• • • • • • •" register={register} setErrorCode={setErrorCode} errorMessage={errors.password && errors.password.message} trigger={trigger} watch={watch} />
+        <StringInput type="email" id="email" label="Email address" placeholder="example@example.com" register={register} setErrorMessage={setErrorMessage} errorMessage={errors.email && errors.email.message} trigger={trigger} watch={watch} />
+        <StringInput type="password" id="password" label="Password" placeholder="• • • • • • •" register={register} setErrorMessage={setErrorMessage} errorMessage={errors.password && errors.password.message} trigger={trigger} watch={watch} />
         <div className="flex items-center justify-between">
           <RoundBtn type="submit" innerText="Login" bgColor="primary-green" textColor="white" />
         </div>
       </form>
-      {loading ? <SmallLoader /> : <p className="text-danger text-xs mt-3">{(errorCode && errorCode <= 401 && "Wrong email or password") || (errorCode && errorCode === 404 && "User not found") || (errorCode && errorCode === 500 && "Server error")}</p>}
+      {loading ? <SmallLoader /> : <p className="text-danger text-xs mt-3">{errorMessage && errorMessage}</p>}
       <Link to="/#" className=" w-full block underline mt-4 text-primary-green hover:text-primary-blue">
         Forgot your password?
       </Link>
