b6fae8c0ea6d3c192d085ced5f62453aeca293c0
Thea <theola51924@stud.noroff.no>
2024-11-18 12:53:28 +0700

    fixed the venueManager saving issue and removed old comments

--- src/hooks/useAuthedFetch.jsx
+++ src/hooks/useAuthedFetch.jsx
@@ -1,11 +1,10 @@
 import { useState } from "react";
 import useAuthStore from "../stores/useAuthStore";
-import { data } from "autoprefixer";
 const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
 const apiKey = import.meta.env.VITE_API_KEY;
 
 export default function useAuthedFetch() {
-  const { accessToken } = useAuthStore();
+  const { accessToken, setVenueManager } = useAuthStore();
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
@@ -22,7 +21,6 @@ export default function useAuthedFetch() {
 
     if (!response.ok) {
       const errorData = await response.json();
-      //consle.log for debugging
       console.log("errror catched in apiCall", errorData);
       const error = new Error("Error when calling API");
       error.data = errorData; // Attach the original error object
@@ -39,24 +37,19 @@ export default function useAuthedFetch() {
       const response = await callApiWith(apiBaseUrl + additionalRefs, {
         method: "GET",
       });
-      console.log("response:", response);
-      // setAccessToken(response.data.accessToken);
-      // setUserName(response.data.name);
-      // setVenueManager(response.data.venueManager);
+      console.log("response from authedFetch:", response);
+      setVenueManager(response.data.venueManager);
       return { success: true, data: response.data };
-      // Handle any other logic like saving token, redirecting, etc.
     } catch (err) {
       console.log("error catched in fetch user func", err.data);
-      // setError(err.data);
       return { success: false, error: err.data };
     } finally {
-      // setLoading(false);
+      setLoading(false);
     }
   };
 
   return {
     loading,
-    setLoading,
     error,
     fetchUser,
   };
