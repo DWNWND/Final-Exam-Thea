be0788c2906839fed96ae2dc68b41a7aab837f85
Thea <theola51924@stud.noroff.no>
2024-11-18 12:53:06 +0700

    fixed some naming conventions

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -1,29 +1,26 @@
 import { useState, useEffect } from "react";
 
-// const apiKey = import.meta.env.VITE_API_KEY;
-
 export default function useFetch(url, headers) {
   const [data, setData] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [isError, setIsError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     async function getData() {
       try {
-        setIsLoading(true);
-        setIsError(false);
+        setLoading(true);
+        setError(false);
         const fetchedData = await fetch(url, headers);
         const json = await fetchedData.json();
         setData(json);
       } catch (error) {
-        console.log(error);
-        setIsError(true);
+        console.log("error catched in the useFetch hook:", error);
+        setError(true);
       } finally {
-        // setIsLoading(false);
+        setLoading(false);
       }
     }
     getData();
   }, [url]);
-  return { data, setIsLoading, isLoading, isError };
+  return { data, loading, setLoading, error };
 }
-
