a1551ebfb4c864f0969a1e7889bb7116f19f8408
Thea <theola51924@stud.noroff.no>
2024-11-20 20:39:12 +0700

    did the same with the listOgListings and separated the component from the route

---
+++ src/components/User/ListOfListings/index.jsx
@@ -0,0 +1,67 @@
+import VenueCard from "../../Venues/VenueCard";
+import { useAuthStore } from "../../../stores";
+import { useEffect } from "react";
+import { useState } from "react";
+import useAuthedFetch from "../../../hooks/useAuthedFetch";
+import { SmallSpinnerLoader } from "../../Loaders";
+import ErrorFallback from "../../ErrorFallback";
+
+export default function ListOfListings() {
+  const [allListings, setAllListings] = useState([]);
+  const [displayedListings, setDisplayedListings] = useState([]);
+  const { accessToken, userName } = useAuthStore();
+  const { loading: loadingInFetch, error: errorInFetch, fetchWithAuthentication } = useAuthedFetch(accessToken);
+  const [mainErrorMessage, setMainErrorMessage] = useState("");
+  const [loadMoreLoader, setLoadMoreLoader] = useState(false);
+
+  const initialDisplayCount = 10;
+
+  const fetchListings = async () => {
+    const response = await fetchWithAuthentication(`/holidaze/profiles/${userName}/venues?_bookings=true`);
+    if (response.success) {
+      setAllListings(response.data);
+    } else {
+      setMainErrorMessage(response.error.errors[0].message);
+    }
+  };
+
+  useEffect(() => {
+    fetchListings();
+  }, []);
+
+  useEffect(() => {
+    setDisplayedListings(allListings.slice(0, initialDisplayCount));
+  }, [allListings]);
+
+  const handleLoadMore = () => {
+    setLoadMoreLoader(true);
+    const newCount = displayedListings.length + 10;
+    setDisplayedListings(allListings.slice(0, newCount));
+    setLoadMoreLoader(false);
+  };
+
+  return (
+    <>
+      {errorInFetch && <ErrorFallback errorMessage={mainErrorMessage} />}
+      <div className="flex flex-col gap-2 bg-comp-green shadow-md p-8 rounded-lg">
+        <h2 className="font-bold text-2xl md:text-3xl text-primary-green uppercase">My active listings</h2>
+        <p className="text-black">{`Showing ${displayedListings.length < allListings.length ? displayedListings.length : allListings.length} of ${allListings.length} ${allListings.length > 1 ? "listings" : "listing"}`}</p>
+        {displayedListings && (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 2xl:grid-cols-3 gap-4 ">
+            {displayedListings.map((listing) => (
+              <VenueCard key={listing.id} venue={listing} myVenues={true} loading={loadingInFetch} />
+            ))}
+          </div>
+        )}
+      </div>
+      {displayedListings && allListings.length > displayedListings.length && (
+        <>
+          <button onClick={handleLoadMore} className="mt-4 px-4 py-2 bg-primary-green text-white rounded-full w-full">
+            Load more listings
+          </button>
+          {loadMoreLoader && <SmallSpinnerLoader />}
+        </>
+      )}
+    </>
+  );
+}
--- src/routes/User/MyListings/index.jsx
+++ src/routes/User/MyListings/index.jsx
@@ -1,28 +1,17 @@
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import {useAuthStore} from "../../../stores";
+import { useAuthStore } from "../../../stores";
 import MainElement from "../../../components/MainElement/index.jsx";
-import ListVenues from "../../../components/User/ListVenues/index.jsx";
-import useAuthedFetch from "../../../hooks/useAuthedFetch.jsx";
-import { useState } from "react";
+import ListOfListings from "../../../components/User/ListOfListings/index.jsx";
 
 export default function MyListings() {
-  const { accessToken, userName } = useAuthStore();
+  const { accessToken } = useAuthStore();
   const navigate = useNavigate();
-  const [userVenues, setUserVenues] = useState([]);
-  const { loading: loadingInFetch, error: errorInFetch, fetchWithAuthentication } = useAuthedFetch(accessToken);
-
-  const fetchVenues = async () => {
-    const response = await fetchWithAuthentication(`/holidaze/profiles/${userName}/venues?_bookings=true`);
-    setUserVenues(response.data);
-  };
 
   useEffect(() => {
     if (!accessToken) {
       navigate("/");
-    } else {
-      fetchVenues();
     }
   }, [accessToken]);
 
@@ -33,8 +22,7 @@ export default function MyListings() {
         <title>My listings | Holidayz</title>
       </Helmet>
       <MainElement>
-        {errorInFetch && <p className="text-danger text-center">We encountered an unexpected issue while processing your request. Please try again later. If the problem persists, contact our support team.</p>}
-        {userVenues.length > 1 && <ListVenues venues={userVenues} maxVenuesShown="4" loading={loadingInFetch} />}
+        <ListOfListings />
       </MainElement>
     </HelmetProvider>
   );
