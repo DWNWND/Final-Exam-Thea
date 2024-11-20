9ec4b568f40263adb1454f1f3fba71b3f0966d0b
Thea <theola51924@stud.noroff.no>
2024-11-20 21:48:49 +0700

    started working on the hooks

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -1,82 +1,62 @@
 import { useState, useEffect } from "react";
+const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
 
 export default function useFetch(url, headers) {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
 
-  useEffect(() => {
-    async function getData() {
-      try {
-        setLoading(true);
-        setError(false);
-        const response = await fetch(url, headers);
-        if (!response.ok) {
-          const errorData = await response.json();
-          console.error("Error logged", errorData.errors[0].message);
-          throw new Error(errorData.errors[0].message || "An error occurred");
-        } else if (response.ok) {
-          const json = await response.json();
-          setData(json);
-        }
-      } catch (error) {
-        console.log("error catched in the useFetch hook:", error);
-        setError(error.message || "An unexpected error occurred");
-        // setError(true);
-      } finally {
-        setLoading(false);
-      }
-    }
-    getData();
-  }, [url]);
-
   // useEffect(() => {
   //   async function getData() {
   //     try {
   //       setLoading(true);
   //       setError(false);
-  //       const fetchedData = await fetch(url, headers);
-  //       const json = await fetchedData.json();
-  //       setData(json);
+  //       const response = await fetch(url, headers);
+  //       if (!response.ok) {
+  //         const errorData = await response.json();
+  //         console.error("Error logged", errorData.errors[0].message);
+  //         throw new Error(errorData.errors[0].message || "An error occurred");
+  //       } else if (response.ok) {
+  //         const json = await response.json();
+  //         setData(json);
+  //         return { success: true, data: response.data };
+  //       }
   //     } catch (error) {
   //       console.log("error catched in the useFetch hook:", error);
-  //       setError(true);
+  //       setError(error.message || "An unexpected error occurred");
+  //       // setError(true);
+  //       return { success: false, error: error.data };
   //     } finally {
   //       setLoading(false);
   //     }
   //   }
   //   getData();
   // }, [url]);
-  return { data, loading, setLoading, error };
+
+  const fetchFromApi = async (additionalRefs) => {
+    setLoading(true);
+    setError(false);
+    try {
+      const response = await fetch(apiBaseUrl + additionalRefs);
+      if (!response.ok) {
+        const errorData = await response.json();
+        console.error("Error logged", errorData.errors[0].message);
+        throw new Error(errorData.errors[0].message || "An error occurred");
+      } else if (response.ok) {
+        const json = await response.json();
+        console.log("json", json);
+        setData(json);
+        return { success: true, data: json.data };
+      }
+    } catch (error) {
+      console.log("error catched  hook:", error);
+      setError(error.message || "An unexpected error occurred");
+      // setError(true);
+      return { success: false, error: error };
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { data, loading, setLoading, error, fetchFromApi };
 }
-
-// const callApiWith = useCallback(
-//   async (url, options = {}) => {
-//     setLoading(true);
-//     setError(null);
-//     try {
-//       const response = await fetch(url, {
-//         ...options,
-//         headers: headers(Boolean(options.body)),
-//       });
-
-//       if (!response.ok) {
-//         const errorData = await response.json();
-//         console.error("Error logged", errorData.errors[0].message);
-//         throw new Error(errorData.errors[0].message || "An error occurred");
-//       } else if (response.ok) {
-//         if (response.status === 204) {
-//           return null;
-//         } else {
-//           return await response.json();
-//         }
-//       }
-//     } catch (err) {
-//       setError(err.message || "An unexpected error occurred");
-//       throw err; // Re-throw the error for additional handling if needed
-//     } finally {
-//       setLoading(false);
-//     }
-//   },
-//   [headers]
-// );
