084d2feeb330a46bac55c3ea55380cd84559aa5c
Thea <theola51924@stud.noroff.no>
2024-11-21 17:08:15 +0700

    exported the new store

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -2,3 +2,4 @@ export { useAuthStore } from "./useAuthStore/";
 export { useBookingDataStore } from "./useBookingDataStore/";
 export { useNavigationStore } from "./useNavigationStore/";
 export { useSearchStore } from "./useSearchStore/";
+export { useTravelDatesStore } from "./useTravelDatesStore/";
