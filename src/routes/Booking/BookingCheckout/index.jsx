41a3f06405a3a3a516d570e7183cf47ead55c380
Thea <theola51924@stud.noroff.no>
2024-11-22 13:09:18 +0700

    fixed page title and conditional rendering

--- src/routes/Booking/BookingCheckout/index.jsx
+++ src/routes/Booking/BookingCheckout/index.jsx
@@ -1,13 +1,27 @@
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import CheckoutForm from "../../../components/Forms/Booking/CheckoutForm";
 import MainElement from "../../../components/MainElement";
+import { useEffect } from "react";
+import { useAuthStore } from "../../../stores";
+import { useNavigate } from "react-router-dom";
+import { useBookingDataStore } from "../../../stores";
 
 export default function BookingCheckout() {
+  const { accessToken } = useAuthStore();
+  const navigate = useNavigate();
+  const { bookingData, selectedVenue } = useBookingDataStore();
+
+  useEffect(() => {
+    if (!accessToken || !bookingData.venueId) {
+      navigate("/");
+    }
+  }, [accessToken]);
+
   return (
     <HelmetProvider>
       <Helmet prioritizeSeoTags>
         <meta name="description" content="" />
-        <title>Booking details | Holidayz</title>
+        <title>{`Booking Checkout | ${selectedVenue.name && selectedVenue.name} | Holidaze`}</title>
       </Helmet>
       <MainElement tailw="flex justify-center">
         <CheckoutForm />
--- src/routes/Booking/BookingDetails/index.jsx
+++ src/routes/Booking/BookingDetails/index.jsx
@@ -1,18 +1,28 @@
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import { useNavigate } from "react-router-dom";
-import {useAuthStore} from "../../../stores";
+import { useAuthStore } from "../../../stores";
 import DetailsForm from "../../../components/Forms/Booking/DetailsForm";
 import MainElement from "../../../components/MainElement/index.jsx";
+import { useEffect } from "react";
+import { useBookingDataStore } from "../../../stores";
 
 export default function BookingDetails() {
-  const { accessToken, userName } = useAuthStore();
+  const { accessToken } = useAuthStore();
   const navigate = useNavigate();
+
+  const { bookingData, selectedVenue } = useBookingDataStore();
+
+  useEffect(() => {
+    if (!accessToken || !bookingData.venueId) {
+      navigate("/");
+    }
+  }, [accessToken]);
 
   return (
     <HelmetProvider>
       <Helmet prioritizeSeoTags>
         <meta name="description" content="" />
-        <title>Booking details | Holidayz</title>
+        <title>{`Booking Details | ${selectedVenue.name && selectedVenue.name} | Holidaze`}</title>
       </Helmet>
       <MainElement tailw="flex justify-center">
         <DetailsForm />
