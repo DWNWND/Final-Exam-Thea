a2047bbdc0acc39bb188ab65af9d895f1fe4577e
Thea <theola51924@stud.noroff.no>
2024-11-22 11:24:37 +0700

    updated styling and conditional rendering of the errorFallback comp

--- src/components/ErrorFallback/index.jsx
+++ src/components/ErrorFallback/index.jsx
@@ -1,8 +1,10 @@
 export default function ErrorFallback({ errorMessage }) {
+  console.log("errorfallback", errorMessage);
+
   return (
-    <div>
-      <p className="text-danger text-center">We encountered an unexpected issue while processing your request. Please try again later. If the problem persists, contact our support team.</p>
-      <p className="text-danger text-center">Error: {errorMessage}</p>
+    <div className="bg-white max-w-[50rem] flex flex-col align-center justify-center mx-auto  mt-10 gap-4 rounded-lg p-4 min-m-2">
+      <p className="text-center text-lg">We encountered an unexpected issue while processing your request. Please try again later. If the problem persists, contact our support team.</p>
+      {errorMessage && <p className=" text-danger italic w-full bg-desatBlue p-4 text-center rounded-lg">{errorMessage}</p>}
     </div>
   );
 }
