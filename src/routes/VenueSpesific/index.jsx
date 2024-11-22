498129842056e4151ae641fc7e9e8eb375c0bf41
Thea <theola51924@stud.noroff.no>
2024-11-22 13:07:21 +0700

    added an date initialization to the venueSpesific comp

--- src/routes/VenueSpesific/index.jsx
+++ src/routes/VenueSpesific/index.jsx
@@ -1,10 +1,23 @@
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import SingleVenue from "../../components/Venues/SingleVenue";
 import MainElement from "../../components/MainElement/index.jsx";
-import { useBookingDataStore } from "../../stores";
+import { useBookingDataStore, useTravelDatesStore } from "../../stores";
+import { useEffect } from "react";
 
 export default function VenueSpesific() {
   const { selectedVenue } = useBookingDataStore();
+  const { setInitialDates } = useTravelDatesStore();
+
+  useEffect(() => {
+    const today = new Date();
+    const tomorrow = new Date();
+    tomorrow.setDate(today.getDate() + 1);
+
+    setInitialDates({
+      todayDateObj: today,
+      tomorrowDateObj: tomorrow,
+    });
+  }, []);
 
   return (
     <HelmetProvider>
