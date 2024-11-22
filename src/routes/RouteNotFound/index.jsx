ec8c53617b94bb7d672ffd4ee66c490a67e0b678
Thea <theola51924@stud.noroff.no>
2024-11-22 13:07:47 +0700

    styled the routeNotFound page

--- src/routes/RouteNotFound/index.jsx
+++ src/routes/RouteNotFound/index.jsx
@@ -1,11 +1,17 @@
 import { Helmet, HelmetProvider } from "react-helmet-async";
+import ErrorFallback from "../../components/ErrorFallback";
+import MainElement from "../../components/MainElement";
 
 export default function RouteNotFound() {
+  console.log("RouteNotFound");
+
   <HelmetProvider>
-  <Helmet prioritizeSeoTags>
-    <meta name="description" content="" />
-    <title>Route Not Found</title>
-  </Helmet>
-  return <div className="error">Page not found</div>;
-  </HelmetProvider>
+    <Helmet prioritizeSeoTags>
+      <meta name="description" content="" />
+      <title>Route Not Found | Holidaze</title>
+    </Helmet>
+    <MainElement>
+      <ErrorFallback errorMessage="Page not found" />
+    </MainElement>
+  </HelmetProvider>;
 }
