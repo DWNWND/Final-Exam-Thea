b0b66dbe142d4cf84f08ab507db89734890c3224
Thea <theola51924@stud.noroff.no>
2024-11-22 13:06:56 +0700

    fixed typo

--- src/stores/useBookingDataStore/index.ts
+++ src/stores/useBookingDataStore/index.ts
@@ -45,6 +45,8 @@ export const useBookingDataStore = create<BookingDataStoreState>()(
             venueId: "",
           },
           selectedVenue: {},
+          bookingEmail: "",
+          successfulBookingId: "",
         }),
     }),
     {
