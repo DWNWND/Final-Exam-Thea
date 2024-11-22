7fd98e3726ad056f058e4f2257d0af904f39e7c3
Thea <theola51924@stud.noroff.no>
2024-11-22 11:23:31 +0700

    exported new loaders

--- src/components/Loaders/index.ts
+++ src/components/Loaders/index.ts
@@ -2,3 +2,5 @@ export { BigSpinnerLoader } from "./Spinners/BigSpinnerLoader";
 export { SmallSpinnerLoader } from "./Spinners/SmallSpinnerLoader";
 export { VenueCardSkeletonLoader } from "./Skeletons/VenueCardSkeletonLoader";
 export { DetailsFormSkeletonLoader } from "./Skeletons/DetailsFormSkeletonLoader";
+export { EditListingFormSkeletonLoader } from "./Skeletons/EditListingFormSkeletonLoader";
+export { OccupancyOverviewSkeletonLoader } from "./Skeletons/OccupancyOverviewSkeletonLoader";
