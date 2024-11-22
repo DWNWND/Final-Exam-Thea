35cf055c1a7fa81246ab541b53c62d280adf735e
Thea <theola51924@stud.noroff.no>
2024-11-22 11:22:29 +0700

    fixed page title

--- src/routes/User/EditListing/index.jsx
+++ src/routes/User/EditListing/index.jsx
@@ -1,5 +1,5 @@
 import { Helmet, HelmetProvider } from "react-helmet-async";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuthStore } from "../../../stores";
 import MainElement from "../../../components/MainElement/index.jsx";
@@ -8,6 +8,7 @@ import EditListingForm from "../../../components/Forms/EditListing/index.jsx";
 export default function EditListing() {
   const { accessToken } = useAuthStore();
   const navigate = useNavigate();
+  const [listingName, setListingName] = useState("");
 
   useEffect(() => {
     if (!accessToken) {
@@ -19,10 +20,10 @@ export default function EditListing() {
     <HelmetProvider>
       <Helmet prioritizeSeoTags>
         <meta name="description" content="" />
-        <title>Edit Listing | Holidayz</title>
+        <title>{`Edit Listing | ${listingName && listingName} | Holidaze`}</title>
       </Helmet>
       <MainElement>
-        <EditListingForm />
+        <EditListingForm setListingName={setListingName}/>
       </MainElement>
     </HelmetProvider>
   );
