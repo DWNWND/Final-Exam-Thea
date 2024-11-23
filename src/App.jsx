ba80729b63a9aed355a50645cfeec3128ab78f9c
Thea <theola51924@stud.noroff.no>
2024-11-23 10:30:59 +0700

    added functionality to log the user out if they navigate away or close the tap or window

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import * as routes from "./routes";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ScrollToTop from "./utils/ScrollToTop";
 import "./App.css";
+import { useEffect } from "react";
 import useInactivityTimer from "./hooks/useInactivityTimer";
+import { useAuthStore } from "./stores";
 
 const router = createBrowserRouter([
   {
@@ -32,8 +34,21 @@ const router = createBrowserRouter([
 ]);
 
 export default function App() {
+  const { logOut } = useAuthStore();
   //fix the inactivity timer to redirect to homepage when logging out and to alert inactivity before logging out
   // useInactivityTimer();
+
+
+  //logout when the user closes the tab
+  useEffect(() => {
+    const handleUnload = () => {
+      logOut();
+    };
+    window.addEventListener("unload", handleUnload);
+    return () => {
+      window.removeEventListener("unload", handleUnload);
+    };
+  }, []);
 
   return (
     <RouterProvider router={router}>
