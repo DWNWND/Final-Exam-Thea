15b58b087d0b071d4d66c29a11f3e1f61fa6b986
Thea <theola51924@stud.noroff.no>
2024-11-23 13:07:54 +0700

    added the new hook and updated logic

--- src/components/DataProvider/index.jsx
+++ src/components/DataProvider/index.jsx
@@ -1,34 +1,25 @@
-import useFetch from "../../hooks/useFetch.jsx";
+import { useApiCall } from "../../hooks";
 import { useState, useEffect } from "react";
 import { DataContext } from "../../contexts/index.jsx";
 import { searchableLocations } from "../../assets/locations/searchableLocations.js";
 import { capitalizeWords } from "../../utils/";
 
 export function DataProvider({ children }) {
-  const { data, loading, setLoading, error, fetchFromApi } = useFetch();
+  const { loading, setLoading, error, callApi } = useApiCall();
   const [allListingsArr, setAllListingsArr] = useState([]);
   const [displayedListings, setDisplayedListings] = useState([]);
-  const [mainErrorMessage, setMainErrorMessage] = useState(""); // the errormessage is not added anywhere
-
-  const fetchAllListings = async () => {
-    const response = await fetchFromApi("/holidaze/venues/?sort=created&sortOrder=desc");
-    if (response.success) {
-      setAllListingsArr(response.data);
-    } else {
-      console.log("this is the error that havent been checked in dataProvider:", response.error);
-      setMainErrorMessage(response.error); // this errormessage have not been checked
-    }
-  };
 
   useEffect(() => {
-    setMainErrorMessage("");
+    setLoading(true);
+    const fetchAllListings = async () => {
+      const result = await callApi("/holidaze/venues/?sort=created&sortOrder=desc");
+      setAllListingsArr(result.data);
+    };
     fetchAllListings();
   }, []);
 
   //fetched all venues from the API and filter out the ones with missing location data, so that the search results will be accurate. Would wish that this was already fixed by the api, so that i could fetch it by page ang with a page limit, and not fetch all.
   useEffect(() => {
-    setLoading(true);
-
     if (allListingsArr && allListingsArr.length >= 0) {
       const filteredOutMissingLocations = allListingsArr.filter((venue) => {
         return venue.location.city && venue.location.country;
@@ -45,7 +36,7 @@ export function DataProvider({ children }) {
       setDisplayedListings(filteredOutMissingLocations);
       setLoading(false);
     }
-  }, [data, allListingsArr]);
+  }, [allListingsArr]);
 
   return <DataContext.Provider value={{ displayedListings, setDisplayedListings, loading, error }}>{children}</DataContext.Provider>;
 }
