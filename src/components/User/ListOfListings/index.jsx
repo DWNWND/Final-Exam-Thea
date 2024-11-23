3f5bbcbb08f4fc391296df1fb3e6e644ab079eaf
Thea <theola51924@stud.noroff.no>
2024-11-23 16:13:55 +0700

    updated listofListings with new api hook

--- src/components/User/ListOfListings/index.jsx
+++ src/components/User/ListOfListings/index.jsx
@@ -2,31 +2,25 @@ import VenueCard from "../../Venues/VenueCard";
 import { useAuthStore } from "../../../stores";
 import { useEffect } from "react";
 import { useState } from "react";
-import useAuthedFetch from "../../../hooks/useAuthedFetch";
+import { useApiCall } from "../../../hooks";
 import { SmallSpinnerLoader } from "../../Loaders";
 import ErrorFallback from "../../ErrorFallback";
 
 export default function ListOfListings() {
-  const { accessToken, userName } = useAuthStore();
-  const { loading: loadingInFetch, error: errorInFetch, fetchWithAuthentication } = useAuthedFetch(accessToken);
+  const { userName } = useAuthStore();
+  const { loading, scopedLoader, error, callApi } = useApiCall();
 
   const [allListings, setAllListings] = useState([]);
   const [displayedListings, setDisplayedListings] = useState([]);
-  const [mainErrorMessage, setMainErrorMessage] = useState("");
   const [loadMoreLoader, setLoadMoreLoader] = useState(false);
 
   const initialDisplayCount = 10;
 
-  const fetchListings = async () => {
-    const response = await fetchWithAuthentication(`/holidaze/profiles/${userName}/venues?_bookings=true`);
-    if (response.success) {
-      setAllListings(response.data);
-    } else {
-      setMainErrorMessage(response.error.errors[0].message);
-    }
-  };
-
   useEffect(() => {
+    const fetchListings = async () => {
+      const result = await callApi(`/holidaze/profiles/${userName}/venues?_bookings=true`);
+      setAllListings(result.data);
+    };
     fetchListings();
   }, []);
 
@@ -43,14 +37,14 @@ export default function ListOfListings() {
 
   return (
     <>
-      {errorInFetch && <ErrorFallback errorMessage={mainErrorMessage} />}
+      {error && <ErrorFallback errorMessage={error} />}
       <div className="flex flex-col gap-2 bg-comp-green shadow-md p-8 rounded-lg">
         <h2 className="font-bold text-2xl md:text-3xl text-primary-green uppercase">My active listings</h2>
         <p className="text-black">{`Showing ${displayedListings.length < allListings.length ? displayedListings.length : allListings.length} of ${allListings.length} ${allListings.length > 1 ? "listings" : "listing"}`}</p>
         {displayedListings && (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 2xl:grid-cols-3 gap-4 ">
             {displayedListings.map((listing) => (
-              <VenueCard key={listing.id} venue={listing} myVenues={true} loading={loadingInFetch} />
+              <VenueCard key={listing.id} venue={listing} myVenues={true} loading={loading} />
             ))}
           </div>
         )}
