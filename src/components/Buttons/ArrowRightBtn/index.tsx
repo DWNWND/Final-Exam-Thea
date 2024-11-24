de1cdb72fbb8ae42268f7d28c070ace8aa677b3b
Thea <theola51924@stud.noroff.no>
2024-11-24 19:47:44 +0700

    updated the arrowright btn

--- src/components/Buttons/ArrowRightBtn/index.tsx
+++ src/components/Buttons/ArrowRightBtn/index.tsx
@@ -3,16 +3,16 @@ import { FaArrowRight } from "react-icons/fa6";
 
 interface ArrowRightBtnProps {
   href: string;
-  myVenues?: boolean;
-  myBookings?: boolean;
+  listing?: boolean;
+  booking?: boolean;
   tailw?: string;
 }
 
-export function ArrowRightBtn({ href, myVenues, myBookings, tailw = "" }: ArrowRightBtnProps) {
+export function ArrowRightBtn({ href, listing = false, booking = false, tailw = "" }: ArrowRightBtnProps) {
   return (
     <Link
       to={href}
-      className={`${myVenues && !myBookings ? "text-primary-green border-primary-green" : "text-primary-blue border-primary-blue"} 
+      className={`${listing && "text-primary-green border-primary-green"} ${booking && "text-primary-blue border-primary-blue"} 
         ${tailw} absolute top-2 right-2 z-30 flex justify-center items-center h-9 w-9 text-xl font-bold rounded-full border bg-white bg-opacity-70 hover:bg-white hover:shadow-md`}>
       <FaArrowRight />
     </Link>
