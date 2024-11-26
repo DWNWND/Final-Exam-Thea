c749ca63cbca969dc835411b6ba0681fef640ec6
Thea <theola51924@stud.noroff.no>
2024-11-26 16:27:11 +0700

    added a check to see if you are a venue manager before going to the new listing form

--- src/routes/User/NewListing/index.jsx
+++ src/routes/User/NewListing/index.jsx
@@ -1,19 +1,20 @@
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import {useAuthStore} from "../../../stores";
+import { useAuthStore } from "../../../stores";
 import NewListingForm from "../../../components/Forms/NewListingForm";
 import MainElement from "../../../components/MainElement/index.jsx";
+import { RoundBtn } from "../../../components/Buttons";
 
 export default function NewListing() {
-  const { accessToken, userName } = useAuthStore();
+  const { accessToken, userName, venueManager } = useAuthStore();
   const navigate = useNavigate();
 
   useEffect(() => {
     if (!accessToken) {
-      navigate("/");
+      navigate("/login");
     }
-  }, [accessToken]);
+  }, []);
 
   return (
     <HelmetProvider>
@@ -22,8 +23,17 @@ export default function NewListing() {
         <meta name="description" content="Create and publish your property listing with ease. Start sharing your unique space today and connect with travelers worldwide!" />
       </Helmet>
       <MainElement tailw="bg-comp">
-        <h1 className="hidden uppercase text-2xl text-primary-green text-center mb-6">New Listing</h1>
-        <NewListingForm />
+        {venueManager ? (
+          <section>
+            <h1 className="hidden uppercase text-2xl text-primary-green text-center mb-6">New Listing</h1>
+            <NewListingForm />
+          </section>
+        ) : (
+          <section>
+            <h1 className="uppercase text-2xl text-primary-green text-center mb-6 mt-10">You have to register as a venue manager to publish listings</h1>
+            <RoundBtn innerText="Register as Venue Manager" bgColor="primary-green" tailw="m-auto" textColor="white" clickFunc={() => navigate(`/user/${userName}/settings`)} />
+          </section>
+        )}
       </MainElement>
     </HelmetProvider>
   );
