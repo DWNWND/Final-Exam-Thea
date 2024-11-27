867d5b8fecdac614c79f8f0ad5e229310eae3251
Thea <theola51924@stud.noroff.no>
2024-11-27 17:37:36 +0700

    converted the modals to typescript

---
+++ src/components/Modals/ImageModal/index.tsx
@@ -0,0 +1,39 @@
+import { IoIosClose } from "react-icons/io";
+
+interface ImageModalProps {
+  image: string;
+  alt: string;
+  toggle: () => void;
+}
+
+export function ImageModal({ image, alt, toggle }: ImageModalProps): JSX.Element {
+  return (
+    <div className="fixed inset-0 flex items-center justify-center z-[100]">
+      <div className="relative md:mx-6 z-[100] max-w-screen-lg mx-auto p-4 md:p-10">
+        <button className="absolute top-4 right-4 md:top-10 md:right-10 text-white text-3xl" onClick={toggle}>
+          <IoIosClose />
+        </button>
+        <img src={image} alt={alt} className="rounded-lg object-contain" />
+        {alt && <p className="text-white text-center">Description: {alt}</p>}
+      </div>
+      <div className="absolute h-full w-full bg-black bg-opacity-75 z-[0]" onClick={toggle}></div>
+    </div>
+  );
+}
+
+// import { IoIosClose } from "react-icons/io";
+
+// export function ImageModal({ image, alt, toggle }) {
+//   return (
+//     <div className="fixed inset-0 flex items-center justify-center z-[100]">
+//       <div className="relative md:mx-6 z-[100]  max-w-screen-lg mx-auto p-4 md:p-10">
+//         <button className="absolute top-4 right-4 md:top-10 md:right-10 text-white text-3xl" onClick={toggle}>
+//           <IoIosClose />
+//         </button>
+//         <img src={image} alt={alt} className="rounded-lg object-contain" />
+//         {alt && <p className="text-white text-center">Description: {alt}</p>}
+//       </div>
+//       <div className="absolute h-full w-full bg-black bg-opacity-75 z-[0]" onClick={toggle}></div>
+//     </div>
+//   );
+// }
