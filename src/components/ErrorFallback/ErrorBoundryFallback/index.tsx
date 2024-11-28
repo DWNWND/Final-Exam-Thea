2e7dcd0c4dc8b1f2bdd3a23aceb0867ba5fd32f8
Thea <theola51924@stud.noroff.no>
2024-11-28 15:09:54 +0700

    deleted old code

--- src/components/ErrorFallback/ErrorBoundryFallback/index.tsx
+++ src/components/ErrorFallback/ErrorBoundryFallback/index.tsx
@@ -8,12 +8,3 @@ export default function ErrorBoundryFallback({ error, resetErrorBoundary }: Fall
     </div>
   );
 }
-
-// export default function ErrorFallback({ errorMessage }) {
-//   return (
-//     <div className="bg-white max-w-[50rem] flex flex-col align-center justify-center mx-auto  mt-10 gap-4 rounded-lg p-4 min-m-2 h-full">
-//       <p className="text-center text-lg">We encountered an unexpected issue while processing your request. Please try again later. If the problem persists, contact our support team.</p>
-//       {errorMessage && <p className=" text-danger italic w-full bg-desatBlue p-4 text-center rounded-lg">{errorMessage}</p>}
-//     </div>
-//   );
-// }
--- src/components/ErrorFallback/GeneralErrorFallback/index.tsx
+++ src/components/ErrorFallback/GeneralErrorFallback/index.tsx
@@ -10,12 +10,3 @@ export default function GeneralErrorFallback({ errorMessage }: GeneralErrorFallb
     </div>
   );
 }
-
-// export default function ErrorFallback({ errorMessage }) {
-//   return (
-//     <div className="bg-white max-w-[50rem] flex flex-col align-center justify-center mx-auto  mt-10 gap-4 rounded-lg p-4 min-m-2 h-full">
-//       <p className="text-center text-lg">We encountered an unexpected issue while processing your request. Please try again later. If the problem persists, contact our support team.</p>
-//       {errorMessage && <p className=" text-danger italic w-full bg-desatBlue p-4 text-center rounded-lg">{errorMessage}</p>}
-//     </div>
-//   );
-// }
