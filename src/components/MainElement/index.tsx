09d2b88b9353956f5b3c9fa6be28cef69d831aa9
Thea <theola51924@stud.noroff.no>
2024-11-28 17:51:07 +0700

    double checked and deleted old code

--- src/components/MainElement/index.tsx
+++ src/components/MainElement/index.tsx
@@ -12,32 +12,9 @@ export default function MainElement({ children, tailw = "", noPadding = false }:
 
   return (
     <div className="relative">
-      <div
-        className={`inset-0 bg-black bg-opacity-10 transition-opacity duration-500 ease-in-out ${isMenuOpen ? "fixed opacity-100 z-40 pointer-events-auto" : "opacity-0 pointer-events-none"}`}
-        onClick={() => setIsMenuOpen && setIsMenuOpen(false)} // Optional: Close the menu when clicking the backdrop
-      ></div>
+      <div className={`inset-0 bg-black bg-opacity-10 transition-opacity duration-500 ease-in-out ${isMenuOpen ? "fixed opacity-100 z-40 pointer-events-auto" : "opacity-0 pointer-events-none"}`} onClick={() => setIsMenuOpen && setIsMenuOpen(false)}></div>
 
       <main className={`${!noPadding && "pt-24 p-4 pb-12 md:px-8"} transition duration-1000 ease-in-out min-h-screen bg-desatBlue ${tailw} ${isMenuOpen ? "blur-sm" : "blur-none"}`}>{children}</main>
     </div>
   );
 }
-
-// import { OpenMenuContext } from "../../contexts";
-// import { useContext } from "react";
-
-// export default function MainElement({ children, tailw, noPadding = false }) {
-//   const { isMenuOpen, setIsMenuOpen } = useContext(OpenMenuContext);
-
-//   return (
-//     <div className="relative">
-//       <div
-//         className={`inset-0 bg-black bg-opacity-10 transition-opacity duration-500 ease-in-out ${isMenuOpen ? "fixed opacity-100 z-40 pointer-events-auto" : "opacity-0 pointer-events-none"}`}
-//         onClick={() => setIsMenuOpen(false)} // Optional: Close the menu when clicking the backdrop
-//       ></div>
-
-//       <main className={`${!noPadding && "pt-24 p-4 pb-12 md:px-8"} transition duration-1000 ease-in-out  min-h-screen bg-desatBlue ${tailw} ${isMenuOpen ? "blur-sm" : "blur-none"}`}>{children}</main>
-//     </div>
-//   );
-// }
-
-// //i need pt-24 for profile page
