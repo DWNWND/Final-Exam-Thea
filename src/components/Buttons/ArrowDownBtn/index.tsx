8e06237546327342503c4cac2705019a0fd871cd
Thea <theola51924@stud.noroff.no>
2024-11-28 13:58:01 +0700

    moved the tailw class

--- src/components/Buttons/ArrowDownBtn/index.tsx
+++ src/components/Buttons/ArrowDownBtn/index.tsx
@@ -10,7 +10,7 @@ interface ArrowDownBtnProps {
 }
 
 export function ArrowDownBtn({ innerText = "", tailw = "", clickFunc = () => {}, open = false, link = false, mainSearch = true }: ArrowDownBtnProps) {
-  const buttonClasses = `${tailw} p-1 px-3 w-auto max-w-auto rounded text-nowrap flex items-center justify-center ${link ? "" : "border lg:hover:shadow-md"} cursor-pointer transition duration-300 ease-in-out ${mainSearch && (open ? "border-primary-green text-primary-green" : "hover:text-primary-green hover:border-primary-green border-primary-light text-primary-light")} ${!mainSearch && (open ? "border-white text-white" : "hover:text-white hover:border-white border-comp-purple text-comp-purple")}`;
+  const buttonClasses = `p-1 px-3 w-auto max-w-auto rounded text-nowrap flex items-center justify-center ${link ? "" : "border lg:hover:shadow-md"} cursor-pointer transition duration-300 ease-in-out ${mainSearch && (open ? "border-primary-green text-primary-green" : "hover:text-primary-green hover:border-primary-green border-primary-light text-primary-light")} ${!mainSearch && (open ? "border-white text-white" : "hover:text-white hover:border-white border-comp-purple text-comp-purple")} ${tailw} `;
 
   return (
     <div onClick={clickFunc} className={buttonClasses}>
