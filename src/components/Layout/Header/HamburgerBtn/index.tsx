7e7894f1fe077e9065d0ad9002b81b87daa7b30d
Thea <theola51924@stud.noroff.no>
2024-11-28 16:55:33 +0700

    double checked and deleted old code

--- src/components/Layout/Header/HamburgerBtn/index.tsx
+++ src/components/Layout/Header/HamburgerBtn/index.tsx
@@ -4,36 +4,14 @@ import { OpenMenuContext } from "../../../../contexts/";
 export default function HamburgerBtn(): JSX.Element {
   const { isMenuOpen, setIsMenuOpen } = useContext(OpenMenuContext);
 
-  const onHamburgerMenuClick = (): void => {
-    setIsMenuOpen(!isMenuOpen);
-  };
+  const onHamburgerMenuClick = () => setIsMenuOpen(!isMenuOpen);
 
   return (
     <button type="button" className="w-6 h-6 border-0 p-0 flex justify-center items-center z-[100]" aria-label="Toggle menu" onClick={onHamburgerMenuClick}>
       <div className={isMenuOpen ? "burger burger-squeeze open" : "burger burger-squeeze"}>
-        <span className="sr-only">Menu</span> {/* Accessibility improvement */}
+        <span className="sr-only">Menu</span>
         <div className="burger-lines"></div>
       </div>
     </button>
   );
 }
-
-// import { useContext } from "react";
-// import { OpenMenuContext } from "../../../../contexts";
-
-// export default function HamburgerBtn() {
-//   const { isMenuOpen, setIsMenuOpen } = useContext(OpenMenuContext);
-
-//   function onHamburgerMenuClick() {
-//     setIsMenuOpen(!isMenuOpen);
-//   }
-
-//   return (
-//     <button className={`w-6 h-6 border-0 p-0 flex justify-center items-center z-[100]`} value="button to open and close menu" onClick={() => onHamburgerMenuClick()}>
-//       <div className={isMenuOpen ? "burger burger-squeeze open" : "burger burger-squeeze"}>
-//         <div className="hidden">menu</div>
-//         <div className="burger-lines"></div>
-//       </div>
-//     </button>
-//   );
-// }
