09e15182746aab71d1e2b88aa534123af7077d8c
Thea <theola51924@stud.noroff.no>
2024-11-29 15:12:35 +0700

    added a button to the list page and removed unused currency btn

--- src/components/Layout/FixedBtnDisplay/index.tsx
+++ src/components/Layout/FixedBtnDisplay/index.tsx
@@ -44,17 +44,27 @@ export default function FixedBtnDisplay(): JSX.Element {
   return (
     <div className={`${isMobile && `${isVisible ? "translate-y-0" : "translate-y-full"} fixed bottom-0 w-full shadow-2xl p-3 bg-white`} ${isMobile && isMenuOpen && "translate-y-full"} z-50 transition-transform duration-300`}>
       <ul className="flex flex-row gap-3 md:gap-4" onClick={() => setIsMenuOpen(false)}>
+        {!isMobile && (
+          <li>
+            <Link to="/search">
+              <RoundBtn innerText="Book your next holiday" bgColor="primary-green" borderColor="primary-green" textColor="white" />
+              {/* <SquareBtn innerText="Book your next holiday" tailw="hover:bg-white bg-opacity-50" bgColor="white" textColor="primary-green" borderColor="primary-green" /> */}
+            </Link>
+          </li>
+        )}
         <li className="w-full">
           <Link to={accessToken ? `/user/${userName}/new/listing` : "/login"}>
             <SquareBtn innerText="List your place" tailw="hover:bg-white bg-opacity-50" bgColor="white" textColor="primary-green" borderColor="primary-green" />
           </Link>
         </li>
-        <li className="w-full">
+        {/* <li className="w-full">
           <SquareBtn innerText="Nok" tailw="hover:bg-white bg-opacity-50" bgColor="white" textColor="primary-green" borderColor="primary-green" />
-        </li>
+        </li> */}
         <li className="w-full">
           <Link to={accessToken ? `/user/${userName}` : "/login"} className="w-full">
-            <RoundBtn innerText={accessToken ? "My profile" : "Login"} bgColor="primary-green" borderColor="primary-green" textColor="white" />
+            <SquareBtn innerText={accessToken ? "My profile" : "Login/register"} tailw="hover:bg-white bg-opacity-50" bgColor="white" textColor="primary-green" borderColor="primary-green" />
+
+            {/* <RoundBtn innerText={accessToken ? "My profile" : "Login"} bgColor="primary-green" borderColor="primary-green" textColor="white" /> */}
           </Link>
         </li>
       </ul>
