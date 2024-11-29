ad5104d83324fd06e8b50afc0fc99b1790bba658
Thea <theola51924@stud.noroff.no>
2024-11-29 11:05:46 +0700

    updated the linting rules to ignore the exhaustive-deps

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,6 @@ import reactRefresh from "eslint-plugin-react-refresh";
 import ts from "@typescript-eslint/eslint-plugin"; // Import TypeScript plugin
 import tsParser from "@typescript-eslint/parser"; // Import TypeScript parser
 import globals from "globals";
-// import js from "eslint-plugin-js";
 
 export default [
   { ignores: ["dist"] },
@@ -34,6 +33,7 @@ export default [
       "react/react-in-jsx-scope": "off",
       "react/jsx-no-target-blank": "off",
       "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+      "react-hooks/exhaustive-deps": "off", // Disable exhaustive-deps rule because of unnecessary warnings
     },
   },
 
@@ -43,71 +43,30 @@ export default [
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
-      parser: tsParser, // Use TypeScript parser
+      parser: tsParser,
       parserOptions: {
         ecmaVersion: "latest",
         ecmaFeatures: { jsx: true },
         sourceType: "module",
-        project: "./tsconfig.json", // Reference your tsconfig.json
+        project: "./tsconfig.json",
       },
     },
     settings: { react: { version: "detect" } },
     plugins: {
-      "@typescript-eslint": ts, // TypeScript ESLint plugin
+      "@typescript-eslint": ts,
       react,
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
     },
     rules: {
-      ...ts.configs.recommended.rules, // TypeScript recommended rules
+      ...ts.configs.recommended.rules,
       ...react.configs.recommended.rules,
       ...react.configs["jsx-runtime"].rules,
       ...reactHooks.configs.recommended.rules,
       "react/react-in-jsx-scope": "off",
       "react/jsx-no-target-blank": "off",
       "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+      "react-hooks/exhaustive-deps": "off", // Disable exhaustive-deps rule because of unnecessary warnings
     },
   },
 ];
-
-// import js from "@eslint/js";
-// import globals from "globals";
-// import react from "eslint-plugin-react";
-// import reactHooks from "eslint-plugin-react-hooks";
-// import reactRefresh from "eslint-plugin-react-refresh";
-// import ts from "@typescript-eslint/eslint-plugin";
-// import tsParser from '@typescript-eslint/parser';
-
-// export default [
-//   { ignores: ["dist"] },
-//   {
-//     files: ["**/*.{js,jsx,ts,tsx}"],
-//     languageOptions: {
-//       ecmaVersion: 2020,
-//       globals: globals.browser,
-//       parser: tsParser,
-//       parserOptions: {
-//         ecmaVersion: "latest",
-//         ecmaFeatures: { jsx: true },
-//         sourceType: "module",
-//         project: "./tsconfig.json",
-//       },
-//     },
-//     settings: { react: { version: "18.3" } },
-//     plugins: {
-//       react,
-//       "react-hooks": reactHooks,
-//       "react-refresh": reactRefresh,
-//       "@typescript-eslint": ts,
-//     },
-//     rules: {
-//       ...js.configs.recommended.rules,
-//       ...react.configs.recommended.rules,
-//       ...react.configs["jsx-runtime"].rules,
-//       ...reactHooks.configs.recommended.rules,
-//       ...ts.configs.recommended.rules,
-//       "react/jsx-no-target-blank": "off",
-//       "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
-//     },
-//   },
-// ];
