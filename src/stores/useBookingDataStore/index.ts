92b6239f03b27ebd784acaa413538b6510a2c419
Thea <theola51924@stud.noroff.no>
2024-11-29 11:06:40 +0700

    added a mock of the slectedListing for store

--- src/stores/useBookingDataStore/index.ts
+++ src/stores/useBookingDataStore/index.ts
@@ -1,5 +1,6 @@
 import { create } from "zustand";
 import { persist, createJSONStorage } from "zustand/middleware";
+import { ListingSpesific } from "../../types";
 
 interface BookingDataStore {
   dateFrom: string;
@@ -12,12 +13,12 @@ interface BookingDataStoreState {
   bookingData: BookingDataStore;
   successfulBookingId: string;
   bookingEmail: string;
-  selectedListing: Record<string, any>;
+  selectedListing: ListingSpesific;
   setBookingData: (bookingData: BookingDataStore) => void;
   setSuccessfulBookingId: (successfulBookingId: string) => void;
   setBookingEmail: (bookingEmail: string) => void;
   clearBookingDataStore: () => void;
-  setSelectedListing: (listing: Record<string, any>) => void;
+  setSelectedListing: (listing: ListingSpesific) => void;
 }
 
 export const useBookingDataStore = create<BookingDataStoreState>()(
@@ -31,7 +32,45 @@ export const useBookingDataStore = create<BookingDataStoreState>()(
       },
       successfulBookingId: "",
       bookingEmail: "",
-      selectedListing: {},
+      selectedListing: {
+        id: "",
+        name: "",
+        description: "",
+        media: [],
+        price: 0,
+        maxGuests: 0,
+        rating: 0,
+        created: "",
+        updated: "",
+        meta: {
+          pets: false,
+          parking: false,
+          breakfast: false,
+          wifi: false,
+        },
+        location: {
+          address: "",
+          city: "",
+          zip: "",
+          country: "",
+          continent: "",
+          lat: 0,
+          lng: 0,
+        },
+        owner: {
+          name: "",
+          email: "",
+          bio: "",
+          avatar: {
+            url: "",
+            alt: "",
+          },
+          banner: {
+            url: "",
+            alt: "",
+          },
+        },
+      },
       setBookingData: (bookingData) => set({ bookingData }),
       setSuccessfulBookingId: (successfulBookingId) => set({ successfulBookingId }),
       setBookingEmail: (bookingEmail) => set({ bookingEmail }),
@@ -44,7 +83,45 @@ export const useBookingDataStore = create<BookingDataStoreState>()(
             guests: 0,
             venueId: "",
           },
-          selectedListing: {},
+          selectedListing: {
+            id: "",
+            name: "",
+            description: "",
+            media: [],
+            price: 0,
+            maxGuests: 0,
+            rating: 0,
+            created: "",
+            updated: "",
+            meta: {
+              pets: false,
+              parking: false,
+              breakfast: false,
+              wifi: false,
+            },
+            location: {
+              address: "",
+              city: "",
+              zip: "",
+              country: "",
+              continent: "",
+              lat: 0,
+              lng: 0,
+            },
+            owner: {
+              name: "",
+              email: "",
+              bio: "",
+              avatar: {
+                url: "",
+                alt: "",
+              },
+              banner: {
+                url: "",
+                alt: "",
+              },
+            },
+          },
           bookingEmail: "",
           successfulBookingId: "",
         }),
