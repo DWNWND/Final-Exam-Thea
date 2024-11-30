ec92395f53c3c42012ecd38aa50ab80c2f86a887
Thea <theola51924@stud.noroff.no>
2024-11-30 13:11:35 +0700

    fixed the loaders

--- src/components/Forms/TravelBooking/DetailsForm/index.tsx
+++ src/components/Forms/TravelBooking/DetailsForm/index.tsx
@@ -103,68 +103,72 @@ export default function DetailsForm(): JSX.Element {
 
   return (
     <>
-      {loading && <DetailsFormSkeletonLoader />}
+      {loading ? (
+        <DetailsFormSkeletonLoader />
+      ) : (
+        <>
+          <div className="flex flex-col gap-6 max-w-[50rem] w-full m-4 p-8 bg-white rounded-lg shadow-sm h-full">
+            <div className="w-full flex flex-col gap-1 bg-comp-purple p-4 rounded-lg">
+              <p className="font-semibold">{selectedListing.name}</p>
+              <p>
+                {formattedStartDate} - {formattedEndDate}
+              </p>
+              <p>{bookingData.guests} guests</p>
+              <div className="rounded-full font-bold text-primary-blue">
+                SUM TOTAL: kr {price} ({nights} {nights > 1 ? "nights" : "night"})
+              </div>
+            </div>
+            <div className="w-full my-6">
+              <h1 className="text-2xl uppercase text-primary-green w-full">Your details</h1>
+              <p className="text-sm italic text-primary-blue w-full">Please enter your details to complete your booking</p>
+            </div>
+            <form className="w-full flex flex-col gap-4 md:gap-6" onSubmit={handleSubmit(onSubmit)}>
+              <div className="relative">
+                <label htmlFor="firstName" className="block text-primary-green mb-2">
+                  First name
+                </label>
+                <input disabled={scopedLoader} placeholder="Kari" type="text" id="firstName" {...register("firstName")} className={`focus:border-primary-green active:border-primary-green placeholder:italic placeholder:font-light font-light text-primary-green border active:text-primary-green focus:text-primary-green active:font-normal focus:font-normal border-comp-greenDark rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline resize-none ${errors.firstName?.message && "border-danger"}`} />
+                {errors.firstName?.message && <p className="text-danger text-xs mt-2">{errors.firstName?.message}</p>}
+              </div>
 
-      <div className="flex flex-col gap-6 max-w-[50rem] w-full m-4 p-8 bg-white rounded-lg shadow-sm h-full">
-        <div className="w-full flex flex-col gap-1 bg-comp-purple p-4 rounded-lg">
-          <p className="font-semibold">{selectedListing.name}</p>
-          <p>
-            {formattedStartDate} - {formattedEndDate}
-          </p>
-          <p>{bookingData.guests} guests</p>
-          <div className="rounded-full font-bold text-primary-blue">
-            SUM TOTAL: kr {price} ({nights} {nights > 1 ? "nights" : "night"})
-          </div>
-        </div>
-        <div className="w-full my-6">
-          <h1 className="text-2xl uppercase text-primary-green w-full">Your details</h1>
-          <p className="text-sm italic text-primary-blue w-full">Please enter your details to complete your booking</p>
-        </div>
-        <form className="w-full flex flex-col gap-4 md:gap-6" onSubmit={handleSubmit(onSubmit)}>
-          <div className="relative">
-            <label htmlFor="firstName" className="block text-primary-green mb-2">
-              First name
-            </label>
-            <input disabled={scopedLoader} placeholder="Kari" type="text" id="firstName" {...register("firstName")} className={`focus:border-primary-green active:border-primary-green placeholder:italic placeholder:font-light font-light text-primary-green border active:text-primary-green focus:text-primary-green active:font-normal focus:font-normal border-comp-greenDark rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline resize-none ${errors.firstName?.message && "border-danger"}`} />
-            {errors.firstName?.message && <p className="text-danger text-xs mt-2">{errors.firstName?.message}</p>}
-          </div>
+              <div className="relative">
+                <label htmlFor="lastName" className="block text-primary-green mb-2">
+                  Last name
+                </label>
+                <input disabled={scopedLoader} placeholder="Nordmann" type="text" id="lastName" {...register("lastName")} className={`focus:border-primary-green active:border-primary-green placeholder:italic placeholder:font-light font-light text-primary-green border active:text-primary-green focus:text-primary-green active:font-normal focus:font-normal border-comp-greenDark rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline resize-none ${errors.lastName?.message && "border-danger"}`} />
+                {errors.lastName?.message && <p className="text-danger text-xs mt-2">{errors.lastName?.message}</p>}
+              </div>
 
-          <div className="relative">
-            <label htmlFor="lastName" className="block text-primary-green mb-2">
-              Last name
-            </label>
-            <input disabled={scopedLoader} placeholder="Nordmann" type="text" id="lastName" {...register("lastName")} className={`focus:border-primary-green active:border-primary-green placeholder:italic placeholder:font-light font-light text-primary-green border active:text-primary-green focus:text-primary-green active:font-normal focus:font-normal border-comp-greenDark rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline resize-none ${errors.lastName?.message && "border-danger"}`} />
-            {errors.lastName?.message && <p className="text-danger text-xs mt-2">{errors.lastName?.message}</p>}
-          </div>
+              <div className="relative">
+                <label htmlFor="email" className="block text-primary-green mb-2">
+                  Email address
+                </label>
+                <input disabled={scopedLoader} placeholder="example@example.com" type="email" id="email" {...register("email")} className={`focus:border-primary-green active:border-primary-green placeholder:italic placeholder:font-light font-light text-primary-green border active:text-primary-green focus:text-primary-green active:font-normal focus:font-normal border-comp-greenDark rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline resize-none ${errors.email?.message && "border-danger"}`} />
+                {errors.email?.message && <p className="text-danger text-xs mt-2">{errors.email?.message}</p>}
+              </div>
 
-          <div className="relative">
-            <label htmlFor="email" className="block text-primary-green mb-2">
-              Email address
-            </label>
-            <input disabled={scopedLoader} placeholder="example@example.com" type="email" id="email" {...register("email")} className={`focus:border-primary-green active:border-primary-green placeholder:italic placeholder:font-light font-light text-primary-green border active:text-primary-green focus:text-primary-green active:font-normal focus:font-normal border-comp-greenDark rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline resize-none ${errors.email?.message && "border-danger"}`} />
-            {errors.email?.message && <p className="text-danger text-xs mt-2">{errors.email?.message}</p>}
-          </div>
+              <div className="relative">
+                <label htmlFor="checkIn" className="block text-primary-green mb-2">
+                  Check-in time
+                </label>
+                <input disabled={scopedLoader} placeholder="14:00" type="text" id="checkIn" {...register("checkIn")} className={`focus:border-primary-green active:border-primary-green placeholder:italic placeholder:font-light font-light text-primary-green border active:text-primary-green focus:text-primary-green active:font-normal focus:font-normal border-comp-greenDark rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline resize-none ${errors.checkIn?.message && "border-danger"}`} />
+                {errors.checkIn?.message && <p className="text-danger text-xs mt-2">{errors.checkIn?.message}</p>}
+              </div>
 
-          <div className="relative">
-            <label htmlFor="checkIn" className="block text-primary-green mb-2">
-              Check-in time
-            </label>
-            <input disabled={scopedLoader} placeholder="14:00" type="text" id="checkIn" {...register("checkIn")} className={`focus:border-primary-green active:border-primary-green placeholder:italic placeholder:font-light font-light text-primary-green border active:text-primary-green focus:text-primary-green active:font-normal focus:font-normal border-comp-greenDark rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline resize-none ${errors.checkIn?.message && "border-danger"}`} />
-            {errors.checkIn?.message && <p className="text-danger text-xs mt-2">{errors.checkIn?.message}</p>}
+              <div className="relative">
+                <label htmlFor="specialRequests" className="block text-primary-green mb-2">
+                  Special requests
+                </label>
+                <textarea disabled={scopedLoader} placeholder="Please let us know if you have any special requests" id="specialRequests" {...register("specialRequests")} className={`focus:border-primary-green active:border-primary-green placeholder:italic placeholder:font-light font-light text-primary-green border active:text-primary-green focus:text-primary-green active:font-normal focus:font-normal border-comp-greenDark rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline resize-none ${errors.specialRequests?.message && "border-danger"}`} rows={4} />
+                {errors.specialRequests?.message && <p className="text-danger text-xs mt-2">{errors.specialRequests?.message}</p>}
+              </div>
+              <div className="flex items-center justify-between my-6">
+                <RoundBtn type="submit" innerText="Next" bgColor={isValid ? "primary-blue" : "comp-gray"} textColor={isValid ? "white" : "primary-light"} borderColor={isValid ? "primary-blue" : "comp"} disabled={!isValid} />
+              </div>
+            </form>
           </div>
-
-          <div className="relative">
-            <label htmlFor="specialRequests" className="block text-primary-green mb-2">
-              Special requests
-            </label>
-            <textarea disabled={scopedLoader} placeholder="Please let us know if you have any special requests" id="specialRequests" {...register("specialRequests")} className={`focus:border-primary-green active:border-primary-green placeholder:italic placeholder:font-light font-light text-primary-green border active:text-primary-green focus:text-primary-green active:font-normal focus:font-normal border-comp-greenDark rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline resize-none ${errors.specialRequests?.message && "border-danger"}`} rows={4} />
-            {errors.specialRequests?.message && <p className="text-danger text-xs mt-2">{errors.specialRequests?.message}</p>}
-          </div>
-          <div className="flex items-center justify-between my-6">
-            <RoundBtn type="submit" innerText="Next" bgColor={isValid ? "primary-blue" : "comp-gray"} textColor={isValid ? "white" : "primary-light"} borderColor={isValid ? "primary-blue" : "comp"} disabled={!isValid} />
-          </div>
-        </form>
-      </div>
+        </>
+      )}
     </>
   );
 }
--- src/components/Forms/UserSettingsForm/index.tsx
+++ src/components/Forms/UserSettingsForm/index.tsx
@@ -84,66 +84,71 @@ export default function SettingsForm(): JSX.Element {
 
   return (
     <>
-      {loading && <SettingsFormSkeletonLoader />}
-      {user && (
-        <div className="max-w-[50rem] mx-auto flex items-center flex-col m-4 p-8 bg-white rounded-lg shadow-sm w-full">
-          <div className="flex justify-between items-center w-full mb-6">
-            <h1 className="text-2xl uppercase text-primary-green">My settings</h1>
-          </div>
-          <form className="flex flex-col gap-6 w-full" onSubmit={handleSubmit(onSubmit)}>
-            <div className="flex items-center">
-              <input disabled={scopedLoader} id="venueManager" type="checkbox" defaultChecked={user.venueManager} className="h-6 w-6 cursor-pointer" {...register("venueManager")} />
-              <label htmlFor="venueManager" className={`ml-2 text-nowrap text-primary-green cursor-pointer`}>
-                Register as a venue manager
-              </label>
-            </div>
-            <div>
-              <label htmlFor="bio" className="block text-primary-green mb-2">
-                Bio
-              </label>
-              <input disabled={scopedLoader} type="text" placeholder="hSomething about you" id="bio" {...register("bio")} defaultValue={user.bio} className={`placeholder:italic placeholder:font-light font-light text-primary-green border active:font-normal focus:font-normal border-comp-greenDark rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${errors.bio?.message ? "border-danger" : "focus:border-primary-green active:border-primary-green"}`} />
-              {errors.bio?.message && <p className="text-danger text-xs mt-2">{errors.bio?.message}</p>}
-            </div>
-            <div>
-              <h2 className="mb-2 text-primary-green">Avatar</h2>
-              <div className="flex flex-col gap-4 bg-comp-gray p-4 rounded-xl">
-                <div>
-                  <label htmlFor="avatar.url" className="block text-primary-green mb-2">
-                    Image url
-                  </label>
-                  <input disabled={scopedLoader} type="url" placeholder="https://www.example.com/image.jpg" id="avatar.url" {...register("avatar.url")} defaultValue={user.avatar.url} className={`placeholder:italic placeholder:font-light font-light text-primary-green border active:font-normal focus:font-normal border-comp-greenDark rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${errors.avatar?.url?.message ? "border-danger" : "focus:border-primary-green active:border-primary-green"}`} />
-                </div>
-                <div>
-                  <label htmlFor="avatar.alt" className="block text-primary-green mb-2">
-                    Description
-                  </label>
-                  <input disabled={scopedLoader} placeholder="Our magical cabin" id="avatar.alt" {...register("avatar.alt")} defaultValue={user.avatar.alt} className={`  placeholder:italic placeholder:font-light font-light text-primary-green border active:font-normal focus:font-normal border-comp-greenDark rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${errors.avatar?.alt?.message ? "border-danger" : "focus:border-primary-green active:border-primary-green"}`} />
-                </div>
-                {errors.avatar?.alt?.message && <p className="text-danger text-xs mt-2">{errors.avatar?.alt?.message}</p>}
+      {loading ? (
+        <SettingsFormSkeletonLoader />
+      ) : (
+        <>
+          {user && (
+            <div className="max-w-[50rem] mx-auto flex items-center flex-col m-4 p-8 bg-white rounded-lg shadow-sm w-full">
+              <div className="flex justify-between items-center w-full mb-6">
+                <h1 className="text-2xl uppercase text-primary-green">My settings</h1>
               </div>
-            </div>
-            <div>
-              <h2 className="mb-2 text-primary-green">Banner</h2>
-              <div className="flex flex-col gap-4 bg-comp-gray p-4 rounded-xl">
-                <div>
-                  <label htmlFor="banner.url" className="block text-primary-green mb-2">
-                    Image url
+              <form className="flex flex-col gap-6 w-full" onSubmit={handleSubmit(onSubmit)}>
+                <div className="flex items-center">
+                  <input disabled={scopedLoader} id="venueManager" type="checkbox" defaultChecked={user.venueManager} className="h-6 w-6 cursor-pointer" {...register("venueManager")} />
+                  <label htmlFor="venueManager" className={`ml-2 text-nowrap text-primary-green cursor-pointer`}>
+                    Register as a venue manager
                   </label>
-                  <input disabled={scopedLoader} type="url" placeholder="https://www.example.com/image.jpg" id="banner.url" {...register("banner.url")} defaultValue={user.banner.url} className={`placeholder:italic placeholder:font-light font-light text-primary-green border active:font-normal focus:font-normal border-comp-greenDark rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${errors.banner?.url?.message ? "border-danger" : "focus:border-primary-green active:border-primary-green"}`} />
                 </div>
                 <div>
-                  <label htmlFor="banner.alt" className="block text-primary-green mb-2">
-                    Description
+                  <label htmlFor="bio" className="block text-primary-green mb-2">
+                    Bio
                   </label>
-                  <input disabled={scopedLoader} placeholder="Our magical cabin" id="banner.alt" {...register("banner.alt")} defaultValue={user.banner.alt} className={`  placeholder:italic placeholder:font-light font-light text-primary-green border active:font-normal focus:font-normal border-comp-greenDark rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${errors.banner?.alt?.message ? "border-danger" : "focus:border-primary-green active:border-primary-green"}`} />
+                  <input disabled={scopedLoader} type="text" placeholder="hSomething about you" id="bio" {...register("bio")} defaultValue={user.bio} className={`placeholder:italic placeholder:font-light font-light text-primary-green border active:font-normal focus:font-normal border-comp-greenDark rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${errors.bio?.message ? "border-danger" : "focus:border-primary-green active:border-primary-green"}`} />
+                  {errors.bio?.message && <p className="text-danger text-xs mt-2">{errors.bio?.message}</p>}
                 </div>
-                {errors.banner?.alt?.message && <p className="text-danger text-xs mt-2">{errors.avatar?.alt?.message}</p>}
-              </div>
+                <div>
+                  <h2 className="mb-2 text-primary-green">Avatar</h2>
+                  <div className="flex flex-col gap-4 bg-comp-gray p-4 rounded-xl">
+                    <div>
+                      <label htmlFor="avatar.url" className="block text-primary-green mb-2">
+                        Image url
+                      </label>
+                      <input disabled={scopedLoader} type="url" placeholder="https://www.example.com/image.jpg" id="avatar.url" {...register("avatar.url")} defaultValue={user.avatar.url} className={`placeholder:italic placeholder:font-light font-light text-primary-green border active:font-normal focus:font-normal border-comp-greenDark rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${errors.avatar?.url?.message ? "border-danger" : "focus:border-primary-green active:border-primary-green"}`} />
+                    </div>
+                    <div>
+                      <label htmlFor="avatar.alt" className="block text-primary-green mb-2">
+                        Description
+                      </label>
+                      <input disabled={scopedLoader} placeholder="Our magical cabin" id="avatar.alt" {...register("avatar.alt")} defaultValue={user.avatar.alt} className={`  placeholder:italic placeholder:font-light font-light text-primary-green border active:font-normal focus:font-normal border-comp-greenDark rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${errors.avatar?.alt?.message ? "border-danger" : "focus:border-primary-green active:border-primary-green"}`} />
+                    </div>
+                    {errors.avatar?.alt?.message && <p className="text-danger text-xs mt-2">{errors.avatar?.alt?.message}</p>}
+                  </div>
+                </div>
+                <div>
+                  <h2 className="mb-2 text-primary-green">Banner</h2>
+                  <div className="flex flex-col gap-4 bg-comp-gray p-4 rounded-xl">
+                    <div>
+                      <label htmlFor="banner.url" className="block text-primary-green mb-2">
+                        Image url
+                      </label>
+                      <input disabled={scopedLoader} type="url" placeholder="https://www.example.com/image.jpg" id="banner.url" {...register("banner.url")} defaultValue={user.banner.url} className={`placeholder:italic placeholder:font-light font-light text-primary-green border active:font-normal focus:font-normal border-comp-greenDark rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${errors.banner?.url?.message ? "border-danger" : "focus:border-primary-green active:border-primary-green"}`} />
+                    </div>
+                    <div>
+                      <label htmlFor="banner.alt" className="block text-primary-green mb-2">
+                        Description
+                      </label>
+                      <input disabled={scopedLoader} placeholder="Our magical cabin" id="banner.alt" {...register("banner.alt")} defaultValue={user.banner.alt} className={`  placeholder:italic placeholder:font-light font-light text-primary-green border active:font-normal focus:font-normal border-comp-greenDark rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${errors.banner?.alt?.message ? "border-danger" : "focus:border-primary-green active:border-primary-green"}`} />
+                    </div>
+                    {errors.banner?.alt?.message && <p className="text-danger text-xs mt-2">{errors.avatar?.alt?.message}</p>}
+                  </div>
+                </div>
+                <SquareBtn disabled={scopedLoader} type="submit" innerText="Save changes" tailw="hover:bg-white bg-opacity-50" bgColor="white" textColor="primary-green" borderColor="primary-green" />
+                {scopedLoader ? <SmallSpinnerLoader /> : <p className={`${error ? "text-danger" : "text-primary-green"} text-xs text-center`}>{error || userFeedbackUpdateMessage}</p>}
+              </form>
             </div>
-            <SquareBtn disabled={scopedLoader} type="submit" innerText="Save changes" tailw="hover:bg-white bg-opacity-50" bgColor="white" textColor="primary-green" borderColor="primary-green" />
-            {scopedLoader ? <SmallSpinnerLoader /> : <p className={`${error ? "text-danger" : "text-primary-green"} text-xs text-center`}>{error || userFeedbackUpdateMessage}</p>}
-          </form>
-        </div>
+          )}
+        </>
       )}
     </>
   );
